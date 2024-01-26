4e0f4380c32c0bb54201993a281e3cbb0828b222
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-01-26 17:06:44 -0300

    feat: create prisma deliverymen repository

---
+++ api/src/infra/database/prisma/repositories/prisma-deliverymen-repository.ts
@@ -0,0 +1,55 @@
+import { DeliveryMenRepository } from '@/domain/delivery/application/repositories/deliverymen-repository';
+import { DeliveryMan } from '@/domain/delivery/enterprise/entities/deliveryman';
+import { PrismaService } from '../prisma.service';
+import { PrismaDeliverymanMapper } from '../mappers/prisma-deliveryman-mapper';
+
+export class PrismaDeliverymenRepository implements DeliveryMenRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async findById(id: string): Promise<DeliveryMan | null> {
+    const deliveryman = await this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!deliveryman) {
+      return null;
+    }
+
+    return PrismaDeliverymanMapper.toDomain(deliveryman);
+  }
+
+  async findByCPF(cpf: number): Promise<DeliveryMan | null> {
+    const deliveryman = await this.prisma.user.findUnique({
+      where: {
+        cpf,
+      },
+    });
+
+    if (!deliveryman) {
+      return null;
+    }
+
+    return PrismaDeliverymanMapper.toDomain(deliveryman);
+  }
+
+  async create(deliveryman: DeliveryMan): Promise<void> {
+    const data = PrismaDeliverymanMapper.toPrisma(deliveryman);
+
+    await this.prisma.user.create({
+      data,
+    });
+  }
+
+  async save(deliveryman: DeliveryMan): Promise<void> {
+    const data = PrismaDeliverymanMapper.toPrisma(deliveryman);
+
+    await this.prisma.user.update({
+      where: {
+        id: deliveryman.id.toString(),
+      },
+      data,
+    });
+  }
+}
