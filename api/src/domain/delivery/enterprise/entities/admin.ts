279dbae445d409b82c373e8e5b50e15f55a8150e
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-01-29 19:42:28 -0300

    refactor: add name, email, password and role getters

--- api/src/domain/delivery/enterprise/entities/admin.ts
+++ api/src/domain/delivery/enterprise/entities/admin.ts
@@ -4,10 +4,28 @@ import { Role } from './order';
 
 interface AdminProps {
   name: string;
+  email: string;
+  password: string;
   role: Role.ADMIN;
 }
 
 export class Admin extends Entity<AdminProps> {
+  get name() {
+    return this.props.name;
+  }
+
+  get email() {
+    return this.props.email;
+  }
+
+  get password() {
+    return this.props.password;
+  }
+
+  get role() {
+    return this.props.role;
+  }
+
   static create(props: AdminProps, id?: UniqueEntityID) {
     const admin = new Admin(props, id);
 
