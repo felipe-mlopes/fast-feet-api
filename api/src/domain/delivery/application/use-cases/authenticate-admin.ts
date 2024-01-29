df86de6761db357753008fc449c3120f8bb039d3
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-01-29 19:41:19 -0300

    feat: create authenticate admin use case

---
+++ api/src/domain/delivery/application/use-cases/authenticate-admin.ts
@@ -0,0 +1,54 @@
+import { Either, left, right } from '@/core/either';
+import { AdminRepository } from '../repositories/admin-repository';
+import { HashComparer } from '../../cryptography/hash-comparer';
+import { Encrypter } from '../../cryptography/encrypter';
+import { WrongCredentialsError } from './errors/wrong-credentials-error';
+
+interface AuthenticateAdminUseCaseRequest {
+  email: string;
+  password: string;
+}
+
+type AuthenticateAdminUseCaseResponse = Either<
+  WrongCredentialsError,
+  {
+    accessToken: string;
+  }
+>;
+
+export class AuthenticateAdminUseCase {
+  constructor(
+    private adminRepository: AdminRepository,
+    private hashComparer: HashComparer,
+    private encrypter: Encrypter,
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateAdminUseCaseRequest): Promise<AuthenticateAdminUseCaseResponse> {
+    const admin = await this.adminRepository.findByEmail(email);
+
+    if (!admin) {
+      return left(new Error());
+    }
+
+    const isPasswordValid = await this.hashComparer.compare(
+      password,
+      admin.password,
+    );
+
+    if (!isPasswordValid) {
+      return left(new WrongCredentialsError());
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: admin.id.toString(),
+      role: admin.role,
+    });
+
+    return right({
+      accessToken,
+    });
+  }
+}
