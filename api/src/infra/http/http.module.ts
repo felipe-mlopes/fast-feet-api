052323e082ddf87598402a58d26120f6f0072ea5
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-01-30 21:44:45 -0300

    feat: add database module in imports, create order controller in controllers and create order use case in providers

--- api/src/infra/http/http.module.ts
+++ api/src/infra/http/http.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 
+import { DatabaseModule } from '../database/database.module';
+
+import { CreateOrderController } from './controllers/create-order.controller';
+
+import { CreateOrderUseCase } from '@/domain/delivery/application/use-cases/create-order';
+
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [DatabaseModule],
+  controllers: [CreateOrderController],
+  providers: [CreateOrderUseCase],
 })
 export class HttpModule {}
