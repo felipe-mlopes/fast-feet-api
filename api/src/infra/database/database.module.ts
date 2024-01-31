67a8f09f0f621653cd51ebf6b388f98541b7c83f
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-01-30 21:42:58 -0300

    feat: add orders repository and recipients repository in providers and exports & prisma orders repository and prisma recipients repository in providers

--- api/src/infra/database/database.module.ts
+++ api/src/infra/database/database.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { OrdersRepository } from '@/domain/delivery/application/repositories/orders-repository';
+import { RecipentsRepository } from '@/domain/delivery/application/repositories/recipients-repository';
+import { PrismaOrdersRepository } from './prisma/repositories/prisma-orders-repository';
+import { PrismaRecipientsRepository } from './prisma/repositories/prisma-recipients-repository';
 
 @Module({
   imports: [],
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: OrdersRepository,
+      useClass: PrismaOrdersRepository,
+    },
+    {
+      provide: RecipentsRepository,
+      useClass: PrismaRecipientsRepository,
+    },
+  ],
+  exports: [PrismaService, OrdersRepository, RecipentsRepository],
 })
 export class DatabaseModule {}
