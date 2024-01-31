b2b957dcac3b0a69ad755be1cd2d44944d7535e2
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-01-30 21:37:54 -0300

    test: create vitest config e2e

---
+++ api/vitest.config.e2e.ts
@@ -0,0 +1,29 @@
+import swc from 'unplugin-swc';
+import { configDefaults, defineConfig } from 'vitest/config';
+import tsConfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    exclude: [...configDefaults.exclude, '**/data/pg/**'],
+    alias: {
+      '@src': './src',
+      '@test': './test',
+    },
+    root: './',
+    setupFiles: ['./test/setup-e2e.ts'],
+  },
+  resolve: {
+    alias: {
+      '@src': './src',
+      '@test': './test',
+    },
+  },
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+});
