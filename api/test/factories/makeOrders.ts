0839167f26557f25cbd026388e3d5838393d4bc0
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-01-30 21:45:59 -0300

    test: feat to add order factory class

--- api/test/factories/makeOrders.ts
+++ api/test/factories/makeOrders.ts
@@ -1,3 +1,4 @@
+import { Injectable } from '@nestjs/common';
 import { faker } from '@faker-js/faker';
 
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
@@ -7,6 +8,8 @@ import {
   Role,
   Status,
 } from '@/domain/delivery/enterprise/entities/order';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { PrismaOrderMapper } from '@/infra/database/prisma/mappers/prisma-order-mapper';
 
 export function makeOrder(
   override: Partial<OrderProps> = {},
@@ -28,4 +31,19 @@ export function makeOrder(
   );
 
   return order;
+}
+
+@Injectable()
+export class OrderFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaOrder(data: Partial<OrderProps> = {}): Promise<Order> {
+    const order = makeOrder(data);
+
+    await this.prisma.order.create({
+      data: PrismaOrderMapper.toPrisma(order),
+    });
+
+    return order;
+  }
 }
