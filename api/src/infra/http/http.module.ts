96d561c7c32673c315d1c13a8131822886c1d72c
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-01-31 21:37:54 -0300

    feat: add cryptography module in import & create account controller and authenticate account controller in controllers & register admin use case and authenticate admin use case in providers

--- api/src/infra/http/http.module.ts
+++ api/src/infra/http/http.module.ts
@@ -1,14 +1,27 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '../database/database.module';
+import { CryptographyModule } from '../cryptography/cryptography.module';
 
 import { CreateOrderController } from './controllers/create-order.controller';
+import { CreateAccountController } from './controllers/create-account.controller';
+import { AuthenticateAccountController } from './controllers/authenticate-account.controller';
 
+import { RegisterAdminUseCase } from '@/domain/delivery/application/use-cases/register-admin';
 import { CreateOrderUseCase } from '@/domain/delivery/application/use-cases/create-order';
+import { AuthenticateAdminUseCase } from '@/domain/delivery/application/use-cases/authenticate-admin';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [CreateOrderController],
-  providers: [CreateOrderUseCase],
+  imports: [DatabaseModule, CryptographyModule],
+  controllers: [
+    CreateAccountController,
+    CreateOrderController,
+    AuthenticateAccountController,
+  ],
+  providers: [
+    RegisterAdminUseCase,
+    CreateOrderUseCase,
+    AuthenticateAdminUseCase,
+  ],
 })
 export class HttpModule {}
