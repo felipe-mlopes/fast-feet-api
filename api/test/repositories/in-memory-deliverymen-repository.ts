30cdedc01c1b1f5f3ff0d64d9e1a9a6bc2370069
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-01-31 21:42:53 -0300

    test: refactor to change DeliveryMen to DeliveryMenUser domain & include find by email method

--- api/test/repositories/in-memory-deliverymen-repository.ts
+++ api/test/repositories/in-memory-deliverymen-repository.ts
@@ -1,10 +1,10 @@
 import { DeliveryMenRepository } from '@/domain/delivery/application/repositories/deliverymen-repository';
-import { DeliveryMan } from '@/domain/delivery/enterprise/entities/deliveryman';
+import { DeliveryManUser } from '@/domain/delivery/enterprise/entities/deliveryman-user';
 
 export class InMemoryDeliveryMenRepository implements DeliveryMenRepository {
-  public items: DeliveryMan[] = [];
+  public items: DeliveryManUser[] = [];
 
-  async findById(id: string): Promise<DeliveryMan | null> {
+  async findById(id: string): Promise<DeliveryManUser | null> {
     const deliveryman = this.items.find((item) => item.id.toString() === id);
 
     if (!deliveryman) {
@@ -14,7 +14,7 @@ export class InMemoryDeliveryMenRepository implements DeliveryMenRepository {
     return deliveryman;
   }
 
-  async findByCPF(cpf: number): Promise<DeliveryMan | null> {
+  async findByCPF(cpf: string): Promise<DeliveryManUser | null> {
     const deliveryman = this.items.find((item) => item.cpf === cpf);
 
     if (!deliveryman) {
@@ -24,11 +24,21 @@ export class InMemoryDeliveryMenRepository implements DeliveryMenRepository {
     return deliveryman;
   }
 
-  async create(deliveryman: DeliveryMan): Promise<void> {
+  async findByEmail(email: string): Promise<DeliveryManUser | null> {
+    const deliveryman = this.items.find((item) => item.email === email);
+
+    if (!deliveryman) {
+      return null;
+    }
+
+    return deliveryman;
+  }
+
+  async create(deliveryman: DeliveryManUser): Promise<void> {
     this.items.push(deliveryman);
   }
 
-  async save(deliveryman: DeliveryMan): Promise<void> {
+  async save(deliveryman: DeliveryManUser): Promise<void> {
     const itemIndex = this.items.findIndex(
       (item) => item.id === deliveryman.id,
     );
