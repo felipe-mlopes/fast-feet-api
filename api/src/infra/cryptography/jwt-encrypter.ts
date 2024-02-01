9a0184847439143c00b1c9fcac2d35645dbc07b3
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-02-01 18:02:02 -0300

    feat: create jwt encrypter

---
+++ api/src/infra/cryptography/jwt-encrypter.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+import { Encrypter } from '@/domain/delivery/cryptography/encrypter';
+
+@Injectable()
+export class JwtEncrypter implements Encrypter {
+  constructor(private jwtService: JwtService) {}
+
+  encrypt(payload: Record<string, unknown>): Promise<string> {
+    return this.jwtService.signAsync(payload);
+  }
+}
