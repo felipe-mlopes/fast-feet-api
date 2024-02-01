7bf5def7f9c4e1bd54a7f4fd7f1ec8ed5730c112
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-01-31 21:36:25 -0300

    feat: add admin repository and deliverymen repository in provide & prisma admin repository and prisma deliverymen repository in useClass within providers

--- api/src/infra/database/database.module.ts
+++ api/src/infra/database/database.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+
 import { OrdersRepository } from '@/domain/delivery/application/repositories/orders-repository';
 import { RecipentsRepository } from '@/domain/delivery/application/repositories/recipients-repository';
+import { AdminRepository } from '@/domain/delivery/application/repositories/admin-repository';
+import { DeliveryMenRepository } from '@/domain/delivery/application/repositories/deliverymen-repository';
+
 import { PrismaOrdersRepository } from './prisma/repositories/prisma-orders-repository';
 import { PrismaRecipientsRepository } from './prisma/repositories/prisma-recipients-repository';
+import { PrismaDeliverymenRepository } from './prisma/repositories/prisma-deliverymen-repository';
+import { PrismaAdminRepository } from './prisma/repositories/prisma-admin-repository';
 
 @Module({
   imports: [],
@@ -17,7 +23,21 @@ import { PrismaRecipientsRepository } from './prisma/repositories/prisma-recipie
       provide: RecipentsRepository,
       useClass: PrismaRecipientsRepository,
     },
+    {
+      provide: AdminRepository,
+      useClass: PrismaAdminRepository,
+    },
+    {
+      provide: DeliveryMenRepository,
+      useClass: PrismaDeliverymenRepository,
+    },
   ],
-  exports: [PrismaService, OrdersRepository, RecipentsRepository],
+  exports: [
+    PrismaService,
+    OrdersRepository,
+    RecipentsRepository,
+    AdminRepository,
+    DeliveryMenRepository,
+  ],
 })
 export class DatabaseModule {}
