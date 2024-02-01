f539e8ec3f2d61269eda905d0f900f3447e8a3b7
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-01-31 21:27:53 -0300

    refactor: change the file name to DeliveryManUser and add User as a class extension

---
+++ api/src/domain/delivery/enterprise/entities/deliveryman-user.ts
@@ -0,0 +1,30 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { User, UserProps } from './user';
+import { Optional } from '@/core/types/optional';
+import { Role } from './order';
+
+export interface DeliveryManUserProps extends UserProps {
+  deliveryManId: UniqueEntityID;
+}
+
+export class DeliveryManUser extends User<DeliveryManUserProps> {
+  get deliveryManId() {
+    return this.props.deliveryManId;
+  }
+
+  static create(
+    props: Optional<DeliveryManUserProps, 'createdAt' | 'role'>,
+    id?: UniqueEntityID,
+  ) {
+    const deliveryManUser = new DeliveryManUser(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+        role: Role.USER,
+      },
+      id,
+    );
+
+    return deliveryManUser;
+  }
+}
