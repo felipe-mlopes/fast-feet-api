52411a18730f5872d3d191765d3f245f3a1a7b5a
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-01-31 21:20:08 -0300

    test: add new unit test to not able to register a new admin account with email exists

--- api/src/domain/delivery/application/use-cases/register-admin.spec.ts
+++ api/src/domain/delivery/application/use-cases/register-admin.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryAdminRepository } from 'test/repositories/in-memory-admin-repository';
 import { RegisterAdminUseCase } from './register-admin';
+import { AdminAlreadyExistsError } from './errors/admin-already-exists-error';
 
 let inMemoryAdminRepository: InMemoryAdminRepository;
 let sut: RegisterAdminUseCase;
@@ -10,13 +11,33 @@ describe('Register Recipient', () => {
     sut = new RegisterAdminUseCase(inMemoryAdminRepository);
   });
 
-  it('should be able to register a admin user', async () => {
+  it('should be able to register a admin account', async () => {
     const result = await sut.execute({
       name: 'John Doe',
       email: 'johndoe@example.com',
+      cpf: '99999999999',
       password: '123456',
     });
 
     expect(result.isRight()).toBe(true);
+  });
+
+  it('should not be able to register a new admin account with email exists', async () => {
+    await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      cpf: '99999999999',
+      password: '123456',
+    });
+
+    const result = await sut.execute({
+      name: 'Max',
+      email: 'johndoe@example.com',
+      cpf: '99999999999',
+      password: '123456',
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(AdminAlreadyExistsError);
   });
 });
