0bf1883c457c233249562020f3eb02bb42b672b6
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-02-01 18:01:15 -0300

    feat: add nestjs injectable

--- api/src/infra/auth/jwt-auth.guard.ts
+++ api/src/infra/auth/jwt-auth.guard.ts
@@ -1,9 +1,10 @@
-import { ExecutionContext } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 
 import { IS_PUBLIC_KEY } from './public';
 
+@Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   constructor(private reflector: Reflector) {
     super();
@@ -14,11 +15,9 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
       context.getHandler(),
       context.getClass(),
     ]);
-
     if (isPublic) {
       return true;
     }
-
     return super.canActivate(context);
   }
 }
