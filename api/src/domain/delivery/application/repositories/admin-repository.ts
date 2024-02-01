254ad2bee0e5fe0329e03f0a33317da488687f5c
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-02-01 17:57:47 -0300

    feat: add find by id method

--- api/src/domain/delivery/application/repositories/admin-repository.ts
+++ api/src/domain/delivery/application/repositories/admin-repository.ts
@@ -1,6 +1,7 @@
 import { AdminUser } from '../../enterprise/entities/admin-user';
 
 export abstract class AdminRepository {
+  abstract findById(id: string): Promise<AdminUser | null>;
   abstract findByEmail(email: string): Promise<AdminUser | null>;
   abstract findByCPF(cpf: string): Promise<AdminUser | null>;
   abstract create(admin: AdminUser): Promise<void>;
