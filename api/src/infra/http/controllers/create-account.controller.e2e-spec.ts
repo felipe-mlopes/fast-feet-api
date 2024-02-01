6488924f07d799f2a3f8e98d10aff729d3579495
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-01-31 21:39:37 -0300

    test: implement e2e test to create authenticate account controller

---
+++ api/src/infra/http/controllers/create-account.controller.e2e-spec.ts
@@ -0,0 +1,44 @@
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { AppModule } from '@/infra/app.module';
+import { DatabaseModule } from '@/infra/database/database.module';
+
+describe('Create Account (E2E)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    prisma = moduleRef.get(PrismaService);
+
+    await app.init();
+  });
+
+  test.skip('[POST] /orders', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'John Doe',
+      email: 'john@example.com',
+      cpf: '12345678900',
+      password: '123456',
+    });
+
+    expect(response.statusCode).toBe(201);
+
+    const userOnDatabase = await prisma.user.findUnique({
+      where: {
+        email: 'john@example.com',
+      },
+    });
+
+    expect(userOnDatabase).toBeTruthy();
+  });
+});
