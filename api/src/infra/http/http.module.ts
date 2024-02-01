6d0ac8d8872cd40eb9b496ee6cf11599fcaee1fc
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-02-01 18:02:49 -0300

    feat: add register deliveryman and register recipient controllers / use cases

--- api/src/infra/http/http.module.ts
+++ api/src/infra/http/http.module.ts
@@ -6,10 +6,14 @@ import { CryptographyModule } from '../cryptography/cryptography.module';
 import { CreateOrderController } from './controllers/create-order.controller';
 import { CreateAccountController } from './controllers/create-account.controller';
 import { AuthenticateAccountController } from './controllers/authenticate-account.controller';
+import { RegisterDeliveryManController } from './controllers/register-deliveryman.controller';
+import { RegisterRecipientController } from './controllers/register-recipient.controller';
 
 import { RegisterAdminUseCase } from '@/domain/delivery/application/use-cases/register-admin';
 import { CreateOrderUseCase } from '@/domain/delivery/application/use-cases/create-order';
 import { AuthenticateAdminUseCase } from '@/domain/delivery/application/use-cases/authenticate-admin';
+import { RegisterDeliverymanUseCase } from '@/domain/delivery/application/use-cases/register-deliveryman';
+import { RegisterRecipientUseCase } from '@/domain/delivery/application/use-cases/register-recipient';
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -17,11 +21,15 @@ import { AuthenticateAdminUseCase } from '@/domain/delivery/application/use-case
     CreateAccountController,
     CreateOrderController,
     AuthenticateAccountController,
+    RegisterDeliveryManController,
+    RegisterRecipientController,
   ],
   providers: [
     RegisterAdminUseCase,
     CreateOrderUseCase,
     AuthenticateAdminUseCase,
+    RegisterDeliverymanUseCase,
+    RegisterRecipientUseCase,
   ],
 })
 export class HttpModule {}
