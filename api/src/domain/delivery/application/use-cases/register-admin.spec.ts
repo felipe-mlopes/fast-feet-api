92e8680423b4b0a95d90f3b101ad7b1fcc4e529f
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-02-15 08:18:24 -0300

    test: refactor to include fake hasher in register admin use case & feat to add new unit test for hash password

--- api/src/domain/delivery/application/use-cases/register-admin.spec.ts
+++ api/src/domain/delivery/application/use-cases/register-admin.spec.ts
@@ -1,14 +1,19 @@
-import { InMemoryAdminRepository } from 'test/repositories/in-memory-admin-repository';
 import { RegisterAdminUseCase } from './register-admin';
+
+import { InMemoryAdminRepository } from 'test/repositories/in-memory-admin-repository';
+import { FakeHasher } from 'test/cryptography/fake-hasher';
+
 import { AdminAlreadyExistsError } from './errors/admin-already-exists-error';
 
 let inMemoryAdminRepository: InMemoryAdminRepository;
+let hashGenerator: FakeHasher;
 let sut: RegisterAdminUseCase;
 
 describe('Register Recipient', () => {
   beforeEach(() => {
     inMemoryAdminRepository = new InMemoryAdminRepository();
-    sut = new RegisterAdminUseCase(inMemoryAdminRepository);
+    hashGenerator = new FakeHasher();
+    sut = new RegisterAdminUseCase(inMemoryAdminRepository, hashGenerator);
   });
 
   it('should be able to register a admin account', async () => {
@@ -39,5 +44,19 @@ describe('Register Recipient', () => {
 
     expect(result.isLeft()).toBe(true);
     expect(result.value).toBeInstanceOf(AdminAlreadyExistsError);
+  });
+
+  it('should be hash admin account password upon registration', async () => {
+    const result = await sut.execute({
+      name: 'Max',
+      email: 'johndoe@example.com',
+      cpf: '99999999999',
+      password: '123456',
+    });
+
+    const hashedPassword = await hashGenerator.hash('123456');
+
+    expect(result.isRight()).toBe(true);
+    expect(inMemoryAdminRepository.items[0].password).toEqual(hashedPassword);
   });
 });
