1bd2224b07f39fee1eb9ae7f22f4b77460fdca77
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-02-15 08:32:42 -0300

    test: remove environment and watch

--- api/vitest.config.e2e.ts
+++ api/vitest.config.e2e.ts
@@ -5,8 +5,6 @@ import tsConfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   test: {
     include: ['**/*.e2e-spec.ts'],
-    environment: 'node',
-    watch: true,
     globals: true,
     root: './',
     setupFiles: ['./test/setup-e2e.ts'],
