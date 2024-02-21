4506ba93a48b82ef15d6a94bcae1833d0c26d22e
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-02-21 06:23:58 -0300

    test: refactor to change zod env to process.env in database url

--- api/test/setup-e2e.ts
+++ api/test/setup-e2e.ts
@@ -29,7 +29,7 @@ function generateUniqueDatabaseURL(schemaId: string) {
 beforeAll(async () => {
   const databaseURL = generateUniqueDatabaseURL(schemaId);
 
-  env.DATABASE_URL = databaseURL;
+  process.env.DATABASE_URL = databaseURL;
 
   execSync('npx prisma migrate deploy');
 });
