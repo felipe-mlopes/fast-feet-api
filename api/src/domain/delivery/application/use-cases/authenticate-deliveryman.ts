066bbaaf0528be118f244f5e7eafc037e7318296
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-02-25 20:49:52 -0300

    feat: create authenticate deliveryman use case

---
+++ api/src/domain/delivery/application/use-cases/authenticate-deliveryman.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@nestjs/common';
+
+import { DeliveryMenRepository } from '../repositories/deliverymen-repository';
+import { Encrypter } from '../../cryptography/encrypter';
+import { HashComparer } from '../../cryptography/hash-comparer';
+
+import { Either, left, right } from '@/core/either';
+import { WrongCredentialsError } from './errors/wrong-credentials-error';
+
+interface AuthenticateDeliverymenUseCaseRequest {
+  email: string;
+  password: string;
+}
+
+type AuthenticateDeliverymenUseCaseResponse = Either<
+  WrongCredentialsError,
+  {
+    accessToken: string;
+  }
+>;
+
+@Injectable()
+export class AuthenticateDeliverymenUseCase {
+  constructor(
+    private deliverymenRepository: DeliveryMenRepository,
+    private hashComparer: HashComparer,
+    private encrypter: Encrypter,
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateDeliverymenUseCaseRequest): Promise<AuthenticateDeliverymenUseCaseResponse> {
+    const deliveryman = await this.deliverymenRepository.findByEmail(email);
+
+    if (!deliveryman) {
+      return left(new WrongCredentialsError());
+    }
+
+    const isPasswordValid = await this.hashComparer.compare(
+      password,
+      deliveryman.password,
+    );
+
+    if (!isPasswordValid) {
+      return left(new WrongCredentialsError());
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: deliveryman.id.toString(),
+    });
+
+    return right({
+      accessToken,
+    });
+  }
+}
