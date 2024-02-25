ba1c48f6b012aadd5c9cbbdd0dc067c28c804730
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-02-25 20:58:59 -0300

    feat: add FetchNearbyOrdersWaitingAndPicknUp, FetchNearbyOrdersDone and AuthenticateDeliverymen controllers and use cases

--- api/src/infra/http/http.module.ts
+++ api/src/infra/http/http.module.ts
@@ -8,12 +8,18 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { AuthenticateAccountController } from './controllers/authenticate-account.controller';
 import { RegisterDeliveryManController } from './controllers/register-deliveryman.controller';
 import { RegisterRecipientController } from './controllers/register-recipient.controller';
+import { FecthNearbyOrdersWaitingAndPicknUpController } from './controllers/fetch-nearby-orders-waiting-and-pickn-up.controller';
+import { FecthNearbyOrdersDoneController } from './controllers/fetch-nearby-orders-done.controller';
+import { AuthenticateDeliverymanController } from './controllers/authenticate-deliveryman.controller';
 
 import { RegisterAdminUseCase } from '@/domain/delivery/application/use-cases/register-admin';
 import { CreateOrderUseCase } from '@/domain/delivery/application/use-cases/create-order';
 import { AuthenticateAdminUseCase } from '@/domain/delivery/application/use-cases/authenticate-admin';
 import { RegisterDeliverymanUseCase } from '@/domain/delivery/application/use-cases/register-deliveryman';
 import { RegisterRecipientUseCase } from '@/domain/delivery/application/use-cases/register-recipient';
+import { FetchNearbyOrdersWaitingAndPicknUpUseCase } from '@/domain/delivery/application/use-cases/fetch-nearby-orders-waiting-and-pickn-up';
+import { FetchNearbyOrdersDoneUseCase } from '@/domain/delivery/application/use-cases/fetch-nearby-orders-done';
+import { AuthenticateDeliverymenUseCase } from '@/domain/delivery/application/use-cases/authenticate-deliveryman';
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -23,6 +29,9 @@ import { RegisterRecipientUseCase } from '@/domain/delivery/application/use-case
     AuthenticateAccountController,
     RegisterDeliveryManController,
     RegisterRecipientController,
+    FecthNearbyOrdersWaitingAndPicknUpController,
+    FecthNearbyOrdersDoneController,
+    AuthenticateDeliverymanController,
   ],
   providers: [
     RegisterAdminUseCase,
@@ -30,6 +39,9 @@ import { RegisterRecipientUseCase } from '@/domain/delivery/application/use-case
     AuthenticateAdminUseCase,
     RegisterDeliverymanUseCase,
     RegisterRecipientUseCase,
+    FetchNearbyOrdersWaitingAndPicknUpUseCase,
+    FetchNearbyOrdersDoneUseCase,
+    AuthenticateDeliverymenUseCase,
   ],
 })
 export class HttpModule {}
