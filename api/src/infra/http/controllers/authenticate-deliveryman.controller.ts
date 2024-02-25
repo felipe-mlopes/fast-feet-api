762061b55b5b494b7ed98d80bedfd6e08a7a7ff4
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-02-25 20:59:34 -0300

    feat: create authenticate deliveryman controller

---
+++ api/src/infra/http/controllers/authenticate-deliveryman.controller.ts
@@ -0,0 +1,61 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  UnauthorizedException,
+  UsePipes,
+} from '@nestjs/common';
+import { z } from 'zod';
+
+import { Public } from '@/infra/auth/public';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe';
+
+import { AuthenticateDeliverymenUseCase } from '@/domain/delivery/application/use-cases/authenticate-deliveryman';
+import { WrongCredentialsError } from '@/domain/delivery/application/use-cases/errors/wrong-credentials-error';
+
+const authenticateBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>;
+
+@Controller('/deliveryman/sessions')
+@Public()
+@UsePipes(new ZodValidationPipe(authenticateBodySchema))
+export class AuthenticateDeliverymanController {
+  constructor(
+    private authenticateAdmin: AuthenticateDeliverymenUseCase,
+    private prisma: PrismaService,
+  ) {}
+
+  @Post()
+  async handle(@Body() body: AuthenticateBodySchema) {
+    const { email, password } = body;
+
+    const result = await this.authenticateAdmin.execute({
+      email,
+      password,
+    });
+
+    if (result.isLeft()) {
+      const error = result.value;
+
+      switch (error.constructor) {
+        case WrongCredentialsError:
+          throw new UnauthorizedException(error.message);
+        default:
+          throw new BadRequestException(error.message);
+      }
+    }
+
+    const { accessToken } = result.value;
+
+    return {
+      access_token: accessToken,
+    };
+  }
+}
