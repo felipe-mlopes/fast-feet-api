d9c855d489287a1fb28a70f0b222306bff476cbd
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-02-28 06:29:37 -0300

    feat: create prisma attachment repository

---
+++ api/src/infra/database/prisma/repositories/prisma-attachment-repository.ts
@@ -0,0 +1,34 @@
+import { AttachmentRepository } from '@/domain/delivery/application/repositories/attachment-repository';
+import { Attachment } from '@/domain/delivery/enterprise/entities/attachment';
+import { Injectable } from '@nestjs/common';
+
+import { PrismaService } from '../prisma.service';
+
+import { PrismaAttachmentMapper } from '../mappers/prisma-attachment-mapper';
+
+@Injectable()
+export class PrismaAttachmentRepository implements AttachmentRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async findByOrderId(orderId: string): Promise<Attachment | null> {
+    const attachment = this.prisma.attachment.findUnique({
+      where: {
+        orderId,
+      },
+    });
+
+    if (!attachment) {
+      return null;
+    }
+
+    return PrismaAttachmentMapper.toDomain(attachment);
+  }
+
+  async create(attachment: Attachment): Promise<void> {
+    const data = PrismaAttachmentMapper.toPrisma(attachment);
+
+    await this.prisma.attachment.create({
+      data,
+    });
+  }
+}
