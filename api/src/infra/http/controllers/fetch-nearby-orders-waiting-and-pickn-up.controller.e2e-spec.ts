879fd0c4e6289ed052913c499d9bbae236127826
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-02-28 08:16:50 -0300

    test: refactor to add recipient factory, to fix route, to include role in access token & to add city on request

--- api/src/infra/http/controllers/fetch-nearby-orders-waiting-and-pickn-up.controller.e2e-spec.ts
+++ api/src/infra/http/controllers/fetch-nearby-orders-waiting-and-pickn-up.controller.e2e-spec.ts
@@ -6,54 +6,70 @@ import request from 'supertest';
 import { AppModule } from '@/infra/app.module';
 import { DatabaseModule } from '@/infra/database/database.module';
 
+import { Status } from '@/domain/delivery/enterprise/entities/order';
+
 import { DeliverymenFactory } from 'test/factories/make-deliverymen';
+import { RecipientFactory } from 'test/factories/make-recipient';
 import { OrderFactory } from 'test/factories/make-orders';
-import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 
 describe('Fetch Nearby Orders Waiting and Pickn Up (E2E)', () => {
   let app: INestApplication;
   let deliveryFactory: DeliverymenFactory;
+  let recipientFactory: RecipientFactory;
   let orderFactory: OrderFactory;
   let jwt: JwtService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [DeliverymenFactory, OrderFactory],
+      providers: [DeliverymenFactory, RecipientFactory, OrderFactory],
     }).compile();
 
     app = moduleRef.createNestApplication();
 
     deliveryFactory = moduleRef.get(DeliverymenFactory);
+    recipientFactory = moduleRef.get(RecipientFactory);
     orderFactory = moduleRef.get(OrderFactory);
     jwt = moduleRef.get(JwtService);
 
     await app.init();
   });
 
-  test.skip('[GET] /orders', async () => {
+  test('[GET] /orders/pending', async () => {
     const deliveryman = await deliveryFactory.makePrismaDeliveryman();
 
-    const accessToken = jwt.sign({ sub: deliveryman.id.toString() });
+    const accessToken = jwt.sign({
+      sub: deliveryman.id.toString(),
+      role: deliveryman.role,
+    });
+
+    const anotherUser = await deliveryFactory.makePrismaDeliveryman();
+
+    const recipient = await recipientFactory.makePrismaRecipient();
 
     await Promise.all([
       orderFactory.makePrismaOrder({
-        recipientId: new UniqueEntityID('client-01'),
+        recipientId: recipient.id,
+        status: Status.WAITING,
         city: 'somewhere',
       }),
       orderFactory.makePrismaOrder({
-        recipientId: new UniqueEntityID('client-01'),
+        recipientId: recipient.id,
+        status: Status.PICKN_UP,
+        deliverymanId: anotherUser.id,
         city: 'somewhere',
       }),
     ]);
 
     const response = await request(app.getHttpServer())
-      .get(`/orders/somewhere/downtown`)
+      .get('/orders/pending')
       .set('Authorization', `Bearer ${accessToken}`)
       .send({
-        deliverymanId: deliveryman.id.toString(),
+        city: 'somewhere',
         page: 1,
       });
+
+    console.log(response.error);
 
     expect(response.statusCode).toBe(200);
 
