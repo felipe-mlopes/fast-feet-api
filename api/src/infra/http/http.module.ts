5eaaf8730ea061f022fa1c37de0358fbd356329f
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-06 17:35:50 -0300

    feat: add edit order status to pickn up controller in controllers & add edit order status to pickn up use case in providers

--- api/src/infra/http/http.module.ts
+++ api/src/infra/http/http.module.ts
@@ -8,44 +8,48 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { AuthenticateAccountController } from './controllers/authenticate-account.controller';
 import { RegisterDeliveryManController } from './controllers/register-deliveryman.controller';
 import { RegisterRecipientController } from './controllers/register-recipient.controller';
-import { FecthNearbyOrdersWaitingAndPicknUpController } from './controllers/fetch-nearby-orders-waiting-and-pickn-up.controller';
-import { FecthNearbyOrdersDoneController } from './controllers/fetch-nearby-orders-done.controller';
 import { AuthenticateDeliverymanController } from './controllers/authenticate-deliveryman.controller';
 import { GetOrderDetailsController } from './controllers/get-order-details.controller';
+import { FecthNearbyOrdersWaitingAndPicknUpController } from './controllers/fetch-nearby-orders-waiting-and-pickn-up.controller';
+import { FecthNearbyOrdersDoneController } from './controllers/fetch-nearby-orders-done.controller';
+import { EditOrderStatusToPicknUpController } from './controllers/edit-order-status-to-pickn-up.controller';
 
 import { RegisterAdminUseCase } from '@/domain/delivery/application/use-cases/register-admin';
 import { CreateOrderUseCase } from '@/domain/delivery/application/use-cases/create-order';
 import { AuthenticateAdminUseCase } from '@/domain/delivery/application/use-cases/authenticate-admin';
 import { RegisterDeliverymanUseCase } from '@/domain/delivery/application/use-cases/register-deliveryman';
 import { RegisterRecipientUseCase } from '@/domain/delivery/application/use-cases/register-recipient';
-import { FetchNearbyOrdersWaitingAndPicknUpUseCase } from '@/domain/delivery/application/use-cases/fetch-nearby-orders-waiting-and-pickn-up';
-import { FetchNearbyOrdersDoneUseCase } from '@/domain/delivery/application/use-cases/fetch-nearby-orders-done';
 import { AuthenticateDeliverymenUseCase } from '@/domain/delivery/application/use-cases/authenticate-deliveryman';
 import { GetOrderDetailsUseCase } from '@/domain/delivery/application/use-cases/get-order-details';
+import { FetchNearbyOrdersWaitingAndPicknUpUseCase } from '@/domain/delivery/application/use-cases/fetch-nearby-orders-waiting-and-pickn-up';
+import { FetchNearbyOrdersDoneUseCase } from '@/domain/delivery/application/use-cases/fetch-nearby-orders-done';
+import { EditOrderStatusToPicknUpUseCase } from '@/domain/delivery/application/use-cases/edit-order-status-to-pickn-up';
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
   controllers: [
     CreateAccountController,
     CreateOrderController,
-    AuthenticateAccountController,
     RegisterDeliveryManController,
     RegisterRecipientController,
-    FecthNearbyOrdersWaitingAndPicknUpController,
-    FecthNearbyOrdersDoneController,
+    AuthenticateAccountController,
     AuthenticateDeliverymanController,
     GetOrderDetailsController,
+    FecthNearbyOrdersWaitingAndPicknUpController,
+    FecthNearbyOrdersDoneController,
+    EditOrderStatusToPicknUpController,
   ],
   providers: [
     RegisterAdminUseCase,
     CreateOrderUseCase,
-    AuthenticateAdminUseCase,
     RegisterDeliverymanUseCase,
     RegisterRecipientUseCase,
-    FetchNearbyOrdersWaitingAndPicknUpUseCase,
-    FetchNearbyOrdersDoneUseCase,
+    AuthenticateAdminUseCase,
     AuthenticateDeliverymenUseCase,
     GetOrderDetailsUseCase,
+    FetchNearbyOrdersWaitingAndPicknUpUseCase,
+    FetchNearbyOrdersDoneUseCase,
+    EditOrderStatusToPicknUpUseCase,
   ],
 })
 export class HttpModule {}
