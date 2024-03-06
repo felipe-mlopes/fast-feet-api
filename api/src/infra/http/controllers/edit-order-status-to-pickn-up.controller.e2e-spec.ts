f8af1e219efaf08db8f9e5aa9ad1f7cc0875b414
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-06 17:40:59 -0300

    test: create test e2e to edit order status to pick'n up controller

---
+++ api/src/infra/http/controllers/edit-order-status-to-pickn-up.controller.e2e-spec.ts
@@ -0,0 +1,72 @@
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { JwtService } from '@nestjs/jwt';
+import request from 'supertest';
+
+import { AppModule } from '@/infra/app.module';
+import { DatabaseModule } from '@/infra/database/database.module';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+
+import { DeliverymenFactory } from 'test/factories/make-deliverymen';
+import { RecipientFactory } from 'test/factories/make-recipient';
+import { OrderFactory } from 'test/factories/make-orders';
+
+describe('Edit Order Status to Pickn Up (E2E)', () => {
+  let app: INestApplication;
+  let deliveryFactory: DeliverymenFactory;
+  let recipientFactory: RecipientFactory;
+  let orderFactory: OrderFactory;
+  let prisma: PrismaService;
+  let jwt: JwtService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [DeliverymenFactory, RecipientFactory, OrderFactory],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    deliveryFactory = moduleRef.get(DeliverymenFactory);
+    recipientFactory = moduleRef.get(RecipientFactory);
+    orderFactory = moduleRef.get(OrderFactory);
+    prisma = moduleRef.get(PrismaService);
+    jwt = moduleRef.get(JwtService);
+
+    await app.init();
+  });
+
+  test('[GET] /orders/:orderId/picknup', async () => {
+    const recipient = await recipientFactory.makePrismaRecipient();
+
+    const order = await orderFactory.makePrismaOrder({
+      recipientId: recipient.id,
+    });
+
+    console.log(order);
+
+    const orderId = order.id.toString();
+
+    const deliveryman = await deliveryFactory.makePrismaDeliveryman();
+
+    const accessToken = jwt.sign({
+      sub: deliveryman.id.toString(),
+      role: deliveryman.role,
+    });
+
+    const response = await request(app.getHttpServer())
+      .patch(`/orders/${orderId}/picknup`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send();
+
+    expect(response.statusCode).toBe(200);
+
+    const orderOnDatabase = await prisma.order.findUnique({
+      where: {
+        id: orderId,
+      },
+    });
+
+    expect(orderOnDatabase).toBeTruthy();
+  });
+});
