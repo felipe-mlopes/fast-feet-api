09f097871383abe236fa3d76d3af5bbdfc9f1a78
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-07 19:55:02 -0300

    feat: create layout page

---
+++ app/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Fast Feet",
+  description: "A reliable shipping company that gets your packages where they need to be, on time.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
