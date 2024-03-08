cc68344d519badfbce975fb7e7b69c5588b8783e
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-08 18:49:31 -0300

    feat: add button component

---
+++ app/src/components/Button.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { ButtonHTMLAttributes } from "react";
+import { useFormStatus } from "react-dom";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  content: string;
+}
+
+export function Button({ content, ...props }: ButtonProps) {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      className="w-full px-[8.25rem] py-[1.125rem] rounded bg-orange-light text-purple-dark hover:bg-orange-300 font-medium"
+      disabled={pending}
+      {...props}
+    >
+      {pending ? "Carregando..." : content}
+    </button>
+  );
+}
