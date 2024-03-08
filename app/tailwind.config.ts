1c538b243da1d09ae43ff48d031997b186e1a2a0
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-08 18:46:52 -0300

    chore: add new colors in extend

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -14,7 +14,12 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        "indigo-blue": "#4C33CC"
+        "indigo-blue": "#4C33CC",
+        "orange-light": "#FFC042",
+        "lilac-smooth": "#D4CCFF",
+        "purple-dark": "#4C4766",
+        "gray-light": "#F7F5FA",
+        "bluish-gray": "#DAD7E0",
       }
     },
   },
