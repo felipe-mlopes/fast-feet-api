af64ab9c8254775fa6a4d1f80ecfdd113b22ac60
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-08 18:49:18 -0300

    refactor: add main and login form component

--- app/src/app/login/page.tsx
+++ app/src/app/login/page.tsx
@@ -1,14 +1,26 @@
-import Logo from "@/components/Logo";
-import { LogoIcon } from "@/components/LogoIcon";
+import { LoginForm } from "@/components/LoginForm";
+import { Logo } from "@/components/Logo";
+import { LogoIcon } from "@/components/icons/LogoIcon";
 
 export default function Login() {
-    return (
-        <div className="flex flex-col justify-around h-screen bg-indigo-blue">
-            <header className="flex items-center justify-between px-8 pt-10">
-                <LogoIcon />
-                <Logo />
-            </header>
-            <div>Login</div>
+  return (
+    <div className="flex flex-col justify-between h-screen mx-8 my-10">
+      <header className="flex items-center justify-between">
+        <LogoIcon />
+        <Logo />
+      </header>
+      <main>
+        <div className="space-y-4">
+          <h2 className="flex flex-col text-5xl font-bold text-white italic">
+            <span className="text-orange-light">Entregador,</span>
+            <span>você é nosso maior valor</span>
+          </h2>
+          <p className="text-base font-normal text-lilac-smooth mr-28">
+            Faça seu login para começar suas entregas.
+          </p>
         </div>
-    )
+      </main>
+      <LoginForm />
+    </div>
+  );
 }
