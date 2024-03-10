12dcc473d59c1df3550d389f1b8635aa098d1111
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-10 18:13:47 -0300

    feat: add more validations

--- app/src/utils/validation.ts
+++ app/src/utils/validation.ts
@@ -1,6 +1,11 @@
 import { z } from "zod";
 
 export const formSchemaLogin = z.object({
-    cpf: z.string().min(11, { message: "O CPF deve conter 11 números." }).max(11, { message: "O CPF deve conter 11 números." }),
-    password: z.string().min(6, { message: "A senha deve conter no mínimo 6 caracteres." })
+    cpf: z
+        .string()
+        .refine((val) => val.length === 14, { message: "CPF inválido" })
+        .refine((val) => /^\d{3}\.\d{3}\.\d{3}-\d{2}$/.test(val), { message: "CPF inválido" }),
+    password: z
+        .string()
+        .min(6, { message: "A senha deve conter no mínimo 6 caracteres." })
 })
