838e73625b71d430a9f44053969679b1f5c79636
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-10 18:13:30 -0300

    refactor: updtate function

--- app/src/utils/cpfMask.ts
+++ app/src/utils/cpfMask.ts
@@ -1,14 +1,8 @@
 export function cpfMask(value: string) {
-    if (value.length === 11) {
-        const valueTransform = value
-            .replace(/\D/g, '')
-            .replace(/(\d{3})(\d)/, '$1.$2')
-            .replace(/(\d{3})(\d)/, '$1.$2')
-            .replace(/(\d{3})(\d{1,2})/, '$1-$2')
-            .replace(/(-\d{2})\d+?$/, '$1')
-
-        return valueTransform
-    }
-
-    return ''
+    return value
+        .replace(/\D/g, '')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1')
 }
