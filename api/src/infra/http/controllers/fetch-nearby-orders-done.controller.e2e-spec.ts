67e466aca2a9f8aad602bed260f45038152d03dc
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-11 09:36:31 -0300

    test: fix to add prisma service, change make order done factory to make order factory and include news expects

--- api/src/infra/http/controllers/fetch-nearby-orders-done.controller.e2e-spec.ts
+++ api/src/infra/http/controllers/fetch-nearby-orders-done.controller.e2e-spec.ts
@@ -5,60 +5,64 @@ import request from 'supertest';
 
 import { AppModule } from '@/infra/app.module';
 import { DatabaseModule } from '@/infra/database/database.module';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
 
 import { DeliverymenFactory } from 'test/factories/make-deliverymen';
 import { RecipientFactory } from 'test/factories/make-recipient';
-import { OrderDoneFactory } from 'test/factories/make-orders-done';
+import { OrderFactory } from 'test/factories/make-orders';
 
 describe('Fetch Nearby Orders Done (E2E)', () => {
   let app: INestApplication;
   let deliveryFactory: DeliverymenFactory;
   let recipientFactory: RecipientFactory;
-  let orderDoneFactory: OrderDoneFactory;
+  let orderFactory: OrderFactory;
+  let prisma: PrismaService;
   let jwt: JwtService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [DeliverymenFactory, RecipientFactory, OrderDoneFactory],
+      providers: [DeliverymenFactory, RecipientFactory, OrderFactory],
     }).compile();
 
     app = moduleRef.createNestApplication();
 
     deliveryFactory = moduleRef.get(DeliverymenFactory);
     recipientFactory = moduleRef.get(RecipientFactory);
-    orderDoneFactory = moduleRef.get(OrderDoneFactory);
+    orderFactory = moduleRef.get(OrderFactory);
+    prisma = moduleRef.get(PrismaService);
     jwt = moduleRef.get(JwtService);
 
     await app.init();
   });
 
   test('[GET] /orders/done', async () => {
+    const anotherDeliveryman = await deliveryFactory.makePrismaDeliveryman();
+
+    const recipient = await recipientFactory.makePrismaRecipient();
+
+    const order01 = await orderFactory.makePrismaOrder({
+      recipientId: recipient.id,
+      deliverymanId: anotherDeliveryman.id,
+      city: 'somewhere',
+      title: 'order-01',
+    });
+
+    await prisma.order.update({
+      where: {
+        id: order01.id.toString(),
+      },
+      data: {
+        status: 'DONE',
+      },
+    });
+
     const deliveryman = await deliveryFactory.makePrismaDeliveryman();
 
     const accessToken = jwt.sign({
       sub: deliveryman.id.toString(),
       role: deliveryman.role,
     });
-
-    const anotherDeliveryman = await deliveryFactory.makePrismaDeliveryman();
-
-    const recipient = await recipientFactory.makePrismaRecipient();
-
-    await Promise.all([
-      orderDoneFactory.makePrismaOrder({
-        recipientId: recipient.id,
-        deliverymanId: anotherDeliveryman.id,
-        city: 'somewhere',
-        title: 'order-01',
-      }),
-      orderDoneFactory.makePrismaOrder({
-        recipientId: recipient.id,
-        deliverymanId: anotherDeliveryman.id,
-        city: 'somewhere',
-        title: 'order-02',
-      }),
-    ]);
 
     const response = await request(app.getHttpServer())
       .get('/orders/done')
@@ -69,5 +73,13 @@ describe('Fetch Nearby Orders Done (E2E)', () => {
       });
 
     expect(response.statusCode).toBe(200);
+    expect(response.body.orders).toHaveLength(1);
+    expect(response.body.orders).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          title: 'order-01',
+        }),
+      ]),
+    );
   });
 });
