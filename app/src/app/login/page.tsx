ae95914ba14424bd93f77dff4b8e0c4a6dd11b61
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-11 15:55:56 -0300

    refactor: move header to Header component

--- app/src/app/login/page.tsx
+++ app/src/app/login/page.tsx
@@ -1,14 +1,10 @@
+import { Header } from "@/components/Header";
 import { LoginForm } from "@/components/LoginForm";
-import { Logo } from "@/components/Logo";
-import { LogoIcon } from "@/components/icons/LogoIcon";
 
 export default function Login() {
   return (
     <div className="flex flex-col justify-between items-center h-screen mx-8 my-10 lg:grid lg:grid-col-2 lg:grid-row-3 lg:justify-normal">
-      <header className="flex items-center w-full justify-between lg:row-start-1 lg:row-end-1 lg:col-start-1 lg:col-end-2 lg:w-auto lg:justify-start lg:gap-4">
-        <LogoIcon />
-        <Logo />
-      </header>
+      <Header />
       <main className="lg:row-start-2 lg:row-end-3 lg:col-start-1 lg:col-end-1">
         <div className="flex flex-col gap-4 lg:items-center">
           <h2 className="flex flex-col text-5xl font-bold text-white italic">
