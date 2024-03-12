1aa22b5bae49ddcddd260ddfa9c78d3cbb6fb74a
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-12 12:43:06 -0300

    test: feat to create attachment factory

--- api/test/factories/make-attachment.ts
+++ api/test/factories/make-attachment.ts
@@ -1,6 +1,11 @@
+import { Injectable } from '@nestjs/common';
 import { faker } from '@faker-js/faker';
 
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { PrismaAttachmentMapper } from '@/infra/database/prisma/mappers/prisma-attachment-mapper';
+
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+
 import {
   Attachment,
   AttachmentProps,
@@ -21,4 +26,21 @@ export function makeAttachment(
   );
 
   return attachment;
+}
+
+@Injectable()
+export class AttachmentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAttachment(
+    data: Partial<AttachmentProps> = {},
+  ): Promise<Attachment> {
+    const attachment = makeAttachment(data);
+
+    await this.prisma.attachment.create({
+      data: PrismaAttachmentMapper.toPrisma(attachment),
+    });
+
+    return attachment;
+  }
 }
