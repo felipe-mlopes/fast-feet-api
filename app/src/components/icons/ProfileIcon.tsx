524ab52327af29df852679d238c171d636b13f46
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-12 15:36:51 -0300

    refactor: create svg props interface and include in fill styles condition

--- app/src/components/icons/ProfileIcon.tsx
+++ app/src/components/icons/ProfileIcon.tsx
@@ -1,4 +1,8 @@
-export function ProfileIcon() {
+interface SvgProps {
+  isActived?: boolean;
+}
+
+export function ProfileIcon({ isActived = false }: SvgProps) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -10,7 +14,7 @@ export function ProfileIcon() {
       <g clipPath="url(#clip0_139_1175)">
         <path
           d="M12 12C14.21 12 16 10.21 16 8C16 5.79 14.21 4 12 4C9.79 4 8 5.79 8 8C8 10.21 9.79 12 12 12ZM12 14C9.33 14 4 15.34 4 18V20H20V18C20 15.34 14.67 14 12 14Z"
-          fill="#FFC042"
+          fill={isActived ? "#4C33CC" : "#FFC042"}
         />
       </g>
       <defs>
