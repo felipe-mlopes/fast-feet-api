644d20aae2e8e8950b112b883e76d3269573f5f2
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-12 15:30:25 -0300

    feat: add cover component & change styles css in div global and main

--- app/src/app/login/page.tsx
+++ app/src/app/login/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/Header";
+import { Cover } from "@/components/Cover";
 import { LoginForm } from "@/components/LoginForm";
 
 export default function Login() {
   return (
-    <div className="flex flex-col justify-between items-center h-screen mx-8 my-10 lg:grid lg:grid-col-2 lg:grid-row-3 lg:justify-normal">
+    <div className="flex flex-col justify-between items-center gap-24 mx-8 mt-20 lg:grid lg:grid-col-2 lg:grid-row-3 lg:justify-normal">
       <Header />
-      <main className="lg:row-start-2 lg:row-end-3 lg:col-start-1 lg:col-end-1">
+      <Cover />
+      <main className="flex flex-col gap-16 lg:row-start-2 lg:row-end-3 lg:col-start-1 lg:col-end-1">
         <div className="flex flex-col gap-4 lg:items-center">
           <h2 className="flex flex-col text-5xl font-bold text-white italic">
             <span className="text-orange-light">Entregador,</span>
