72e6caed1522053057ff46371ab18519a49c4fb9
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-12 12:34:53 -0300

    feat: create upload and create attachment controller

---
+++ api/src/infra/http/controllers/upload-and-create-attachment.controller.ts
@@ -0,0 +1,65 @@
+import {
+  BadRequestException,
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  Param,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+import { UploadAndCreateAttachmentUseCase } from '@/domain/delivery/application/use-cases/upload-and-create-attachment';
+import { InvalidAttachmentTypeError } from '@/domain/delivery/application/use-cases/errors/invalid-attachment-type-error';
+
+@Controller('/orders/:orderId/attachment')
+export class UploadAndCreateAttachmentController {
+  constructor(
+    private uploadAndCreateAttachment: UploadAndCreateAttachmentUseCase,
+  ) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async handle(
+    @Param('orderId') orderId: string,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({
+            maxSize: 1024 * 1024 * 2, // 2mb
+          }),
+          new FileTypeValidator({
+            fileType: '.(png|jpg|jpeg|pdf)',
+          }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    const result = await this.uploadAndCreateAttachment.execute({
+      orderId,
+      fileName: file.originalname,
+      fileType: file.mimetype,
+      body: file.buffer,
+    });
+
+    if (result.isLeft()) {
+      const error = result.value;
+
+      switch (error.constructor) {
+        case InvalidAttachmentTypeError:
+          throw new BadRequestException(error.message);
+        default:
+          throw new BadRequestException(error.message);
+      }
+    }
+
+    const { attachment } = result.value;
+
+    return {
+      attachmentId: attachment.id.toString(),
+    };
+  }
+}
