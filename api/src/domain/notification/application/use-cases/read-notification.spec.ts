b5c6058d6465f1fc73225555d180f0b8f12a5ac5
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-13 18:16:47 -0300

    test: refactor to include in memory orders repository in dependency & add make order in unit tests

--- api/src/domain/notification/application/use-cases/read-notification.spec.ts
+++ api/src/domain/notification/application/use-cases/read-notification.spec.ts
@@ -1,31 +1,46 @@
 import { ReadNotificationUseCase } from './read-notification';
 
 import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository';
+import { InMemoryOrdersRepository } from 'test/repositories/in-memory-orders-repository';
 
 import { makeNotifications } from 'test/factories/make-notifications';
 
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { NotAllowedError } from '@/core/errors/not-allowed-error';
+import { makeOrder } from 'test/factories/make-orders';
 
 let inMemoryNotificationsRepository: InMemoryNotificationsRepository;
+let inMemoryOrdersRepository: InMemoryOrdersRepository;
 let sut: ReadNotificationUseCase;
 
 describe('Read Notification', () => {
   beforeEach(() => {
     inMemoryNotificationsRepository = new InMemoryNotificationsRepository();
-    sut = new ReadNotificationUseCase(inMemoryNotificationsRepository);
+    inMemoryOrdersRepository = new InMemoryOrdersRepository();
+    sut = new ReadNotificationUseCase(
+      inMemoryNotificationsRepository,
+      inMemoryOrdersRepository,
+    );
   });
 
   it('should be able to read a notification', async () => {
-    const notification = makeNotifications({
+    const order = makeOrder({
       recipientId: new UniqueEntityID('repicient-01'),
+    });
+
+    await inMemoryOrdersRepository.create(order);
+
+    const trackingCode = order.trackingCode;
+
+    const notification = makeNotifications({
+      recipientId: order.recipientId,
     });
 
     await inMemoryNotificationsRepository.create(notification);
 
     const result = await sut.execute({
-      recipientId: notification.recipientId.toString(),
       notificationId: notification.id.toString(),
+      trackingCode,
     });
 
     expect(result.isRight()).toBe(true);
@@ -35,15 +50,15 @@ describe('Read Notification', () => {
   });
 
   it('should not be able to read a notification from another user', async () => {
-    const notification = makeNotifications({
-      recipientId: new UniqueEntityID('repicient-01'),
-    });
+    const order = makeOrder();
+    await inMemoryOrdersRepository.create(order);
 
+    const notification = makeNotifications();
     await inMemoryNotificationsRepository.create(notification);
 
     const result = await sut.execute({
-      recipientId: 'recipient-02',
       notificationId: notification.id.toString(),
+      trackingCode: 'code-01',
     });
 
     expect(result.isLeft()).toBe(true);
