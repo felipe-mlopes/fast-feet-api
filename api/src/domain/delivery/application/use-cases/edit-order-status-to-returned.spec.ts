9b69de0f36e79c7b07ca0e0bff048276e7a5c995
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-13 18:15:14 -0300

    test: refactor to add make deliverymen and include save method of the in memory orders repository

--- api/src/domain/delivery/application/use-cases/edit-order-status-to-returned.spec.ts
+++ api/src/domain/delivery/application/use-cases/edit-order-status-to-returned.spec.ts
@@ -1,59 +1,81 @@
-import { EditOrderStatusToReturnedUseCase } from './edit-order-status-to-returned';
+import { EditOrderStatusToReturnUseCase } from './edit-order-status-to-return';
 
 import { InMemoryOrdersRepository } from 'test/repositories/in-memory-orders-repository';
 import { InMemoryDeliveryMenRepository } from 'test/repositories/in-memory-deliverymen-repository';
 
 import { makeOrder } from 'test/factories/make-orders';
+import { makeDeliverymen } from 'test/factories/make-deliverymen';
 
 import { Status } from '@/domain/delivery/enterprise/entities/order';
 
-import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { NotAllowedError } from '@/core/errors/not-allowed-error';
 
 let inMemoryOrdersRepository: InMemoryOrdersRepository;
 let inMemoryDeliveryMenRepository: InMemoryDeliveryMenRepository;
-let sut: EditOrderStatusToReturnedUseCase;
+let sut: EditOrderStatusToReturnUseCase;
 
 describe('Edit Order Status to Returned', () => {
   beforeEach(() => {
     inMemoryOrdersRepository = new InMemoryOrdersRepository();
     inMemoryDeliveryMenRepository = new InMemoryDeliveryMenRepository();
-    sut = new EditOrderStatusToReturnedUseCase(
+    sut = new EditOrderStatusToReturnUseCase(
       inMemoryOrdersRepository,
       inMemoryDeliveryMenRepository,
     );
   });
 
-  it('should be able to edit order status to returned', async () => {
+  it('should be able to edit order status to return', async () => {
     const order = makeOrder();
-
     await inMemoryOrdersRepository.create(order);
 
-    const newDeliverymanId = new UniqueEntityID();
+    const deliveryman = makeDeliverymen();
+    await inMemoryDeliveryMenRepository.create(deliveryman);
+
+    order.deliverymanId = deliveryman.id;
+    order.status = Status.PICKN_UP;
+
+    await inMemoryOrdersRepository.save(order);
 
     await sut.execute({
       orderId: order.id.toString(),
-      deliverymanId: newDeliverymanId.toString(),
+      deliverymanId: deliveryman.id.toString(),
     });
 
     expect(inMemoryOrdersRepository.items[0].status).toEqual(Status.WAITING);
+    expect(inMemoryOrdersRepository.items[0].isReturned).toEqual(true);
   });
 
-  it("should not be able to edit order status to done before the order is pick'n up", async () => {
+  it('should not be able to edit order status alreadry done', async () => {
     const order = makeOrder();
-
     await inMemoryOrdersRepository.create(order);
 
-    const newDeliverymanId = new UniqueEntityID();
+    const deliveryman = makeDeliverymen();
+    await inMemoryDeliveryMenRepository.create(deliveryman);
 
-    order.deliverymanId = newDeliverymanId;
+    order.deliverymanId = deliveryman.id;
     order.status = Status.DONE;
 
     await inMemoryOrdersRepository.save(order);
 
     const result = await sut.execute({
-      deliverymanId: newDeliverymanId.toString(),
       orderId: order.id.toString(),
+      deliverymanId: deliveryman.id.toString(),
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(NotAllowedError);
+  });
+
+  it("should not be able to return an order before it is pick'n up", async () => {
+    const order = makeOrder();
+    await inMemoryOrdersRepository.create(order);
+
+    const deliveryman = makeDeliverymen();
+    await inMemoryDeliveryMenRepository.create(deliveryman);
+
+    const result = await sut.execute({
+      orderId: order.id.toString(),
+      deliverymanId: deliveryman.id.toString(),
     });
 
     expect(result.isLeft()).toBe(true);
