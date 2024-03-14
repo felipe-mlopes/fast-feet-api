90e83ef1322989e1447ad3dc8f2bbf20eb28e1f3
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-14 20:55:44 -0300

    refactor: remove unused save method

--- api/src/domain/notification/application/repositories/notifications-repository.ts
+++ api/src/domain/notification/application/repositories/notifications-repository.ts
@@ -3,5 +3,4 @@ import { Notification } from '../../enterprise/entities/notification';
 export abstract class NotificationsRepository {
   abstract findById(id: string): Promise<Notification | null>;
   abstract create(notification: Notification): Promise<void>;
-  abstract save(notification: Notification): Promise<void>;
 }
