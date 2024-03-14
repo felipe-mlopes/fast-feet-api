fb5737f96704300ca0da3e261385e112403d3360
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-14 20:51:25 -0300

    chore: remove read_at in notification table

---
+++ api/prisma/migrations/20240314180928_remove_reat_at_in_notification_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `read_at` on the `notifications` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "notifications" DROP COLUMN "read_at";
