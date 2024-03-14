9cfe352c71ff33aa4e317a6c4ea9df9172b95a9a
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-14 20:59:11 -0300

    refactor: add recipient repository and send email as dependencies & implement find by id method of the recipient repository and send method of the send email

--- api/src/domain/notification/application/use-cases/send-notification.ts
+++ api/src/domain/notification/application/use-cases/send-notification.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 import { NotificationsRepository } from '../repositories/notifications-repository';
+import { RecipentsRepository } from '@/domain/delivery/application/repositories/recipients-repository';
+import { SendEmail } from '../mailing/sendEmail';
 
 import { Notification } from '../../enterprise/entities/notification';
 
-import { Either, right } from '@/core/either';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { Either, left, right } from '@/core/either';
+import { NotAllowedError } from '@/core/errors/not-allowed-error';
 
 export interface SendNotificationUseCaseRequest {
   recipientId: string;
@@ -13,7 +16,7 @@ export interface SendNotificationUseCaseRequest {
 }
 
 export type SendNotificationUseCaseResponse = Either<
-  null,
+  NotAllowedError,
   {
     notification: Notification;
   }
@@ -21,18 +24,34 @@ export type SendNotificationUseCaseResponse = Either<
 
 @Injectable()
 export class SendNotificationUseCase {
-  constructor(private notificationsRepository: NotificationsRepository) {}
+  constructor(
+    private notificationsRepository: NotificationsRepository,
+    private recipientRepository: RecipentsRepository,
+    private sendEmail: SendEmail,
+  ) {}
 
   async execute({
     recipientId,
     title,
   }: SendNotificationUseCaseRequest): Promise<SendNotificationUseCaseResponse> {
+    const recipient = await this.recipientRepository.findById(recipientId);
+
+    if (!recipient) {
+      return left(new NotAllowedError());
+    }
+
     const notification = Notification.create({
       recipientId: new UniqueEntityID(recipientId),
       title,
     });
 
     await this.notificationsRepository.create(notification);
+
+    await this.sendEmail.send({
+      to: [recipient.email],
+      subject: notification.title,
+      html: 'E-mail enviado com sucesso',
+    });
 
     return right({
       notification,
