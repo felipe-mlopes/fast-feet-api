98ebf681c8568ff7add5bac6b923f520a227a160
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-14 21:04:51 -0300

    feat: add mail module in imports

--- api/src/infra/events/events.module.ts
+++ api/src/infra/events/events.module.ts
@@ -1,10 +1,13 @@
+import { Module } from '@nestjs/common';
+
+import { DatabaseModule } from '../database/database.module';
+import { MailModule } from '../mailing/mail.module';
+
 import { OnChangeOrderStatus } from '@/domain/notification/application/subscribes/on-change-order-status';
 import { SendNotificationUseCase } from '@/domain/notification/application/use-cases/send-notification';
-import { Module } from '@nestjs/common';
-import { DatabaseModule } from '../database/database.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, MailModule],
   providers: [OnChangeOrderStatus, SendNotificationUseCase],
 })
 export class EventsModule {}
