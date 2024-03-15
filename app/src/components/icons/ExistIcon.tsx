e04e14ad8abc801e2bd8d9ce2e5504134ea4750b
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-14 21:18:40 -0300

    feat: create exist icon component

---
+++ app/src/components/icons/ExistIcon.tsx
@@ -0,0 +1,25 @@
+export function ExistIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <g clipPath="url(#clip0_53_220)">
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M18 23C19.1046 23 20 22.1046 20 21L20 13L11.33 13L13.91 15.59L12.5 17L7.5 12L12.5 7L13.91 8.41L11.33 11L20 11L20 3C20 1.89543 19.1046 1 18 1L6 0.999999C4.89543 0.999999 4 1.89543 4 3L4 21C4 22.1046 4.89543 23 6 23L18 23Z"
+          fill="#FFC042"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_53_220">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
