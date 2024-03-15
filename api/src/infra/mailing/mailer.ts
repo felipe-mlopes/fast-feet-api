aa6ccd07db57da72e9931e3bd1c3afb27bd221fc
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-14 21:08:11 -0300

    feat: create mailer gateway

---
+++ api/src/infra/mailing/mailer.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+
+import { EnvService } from '../env/env.service';
+
+import {
+  SendEmail,
+  SendEmailParams,
+} from '@/domain/notification/application/mailing/sendEmail';
+
+@Injectable()
+export class Mailer implements SendEmail {
+  private transport: nodemailer.Transporter;
+
+  constructor(private envService: EnvService) {
+    this.transport = nodemailer.createTransport({
+      host: envService.get('MAIL_HOST'),
+      port: envService.get('MAIL_PORT'),
+      secure: false,
+      auth: {
+        user: envService.get('MAIL_USER'),
+        pass: envService.get('MAIL_PASS'),
+      },
+    });
+  }
+
+  async send({ to, subject, html }: SendEmailParams): Promise<void> {
+    try {
+      await this.transport.sendMail({
+        from: this.envService.get('MAIL_FROM'),
+        to,
+        subject,
+        html,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
