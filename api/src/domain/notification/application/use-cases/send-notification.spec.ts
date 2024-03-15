7e3eeb40c1ba7fb13457b52ae373a29a2a39eaae
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-14 21:14:00 -0300

    test: refactor to add in memory recipients repository and fake send email & to include make recipient & to create new test for don't send notification with diferent recipients

--- api/src/domain/notification/application/use-cases/send-notification.spec.ts
+++ api/src/domain/notification/application/use-cases/send-notification.spec.ts
@@ -1,24 +1,55 @@
-import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository';
 import { SendNotificationUseCase } from './send-notification';
 
+import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository';
+import { InMemoryRecipientsRepository } from 'test/repositories/in-memory-recipients-repository';
+import { makeRecipient } from 'test/factories/make-recipient';
+import { NotAllowedError } from '@/core/errors/not-allowed-error';
+import { FakeSendEmail } from 'test/mailing/fake-send-mail';
+
 let inMemoryNotificationsRepository: InMemoryNotificationsRepository;
+let inMemoryRecipientsRepository: InMemoryRecipientsRepository;
+let fakeSendEmail: FakeSendEmail;
 let sut: SendNotificationUseCase;
 
 describe('Send Notification', () => {
   beforeEach(() => {
     inMemoryNotificationsRepository = new InMemoryNotificationsRepository();
-    sut = new SendNotificationUseCase(inMemoryNotificationsRepository);
+    inMemoryRecipientsRepository = new InMemoryRecipientsRepository();
+    fakeSendEmail = new FakeSendEmail();
+    sut = new SendNotificationUseCase(
+      inMemoryNotificationsRepository,
+      inMemoryRecipientsRepository,
+      fakeSendEmail,
+    );
   });
 
   it('should be able to send a notification', async () => {
+    const recipient = makeRecipient();
+
+    await inMemoryRecipientsRepository.create(recipient);
+
     const result = await sut.execute({
-      recipientId: '1',
+      recipientId: recipient.id.toString(),
       title: 'New notification',
     });
 
     expect(result.isRight()).toBe(true);
-    expect(inMemoryNotificationsRepository.items[0]).toEqual(
-      result.value?.notification,
-    );
+    expect(result.value).toEqual({
+      notification: inMemoryNotificationsRepository.items[0],
+    });
+  });
+
+  it('should not be able to send a notification with diferent recipient', async () => {
+    const recipient = makeRecipient();
+
+    await inMemoryRecipientsRepository.create(recipient);
+
+    const result = await sut.execute({
+      recipientId: 'recipient-02',
+      title: 'New notification',
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(NotAllowedError);
   });
 });
