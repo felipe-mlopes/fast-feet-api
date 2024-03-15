6ae0788a5f419172a16e082a5def43a20904e622
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-15 18:47:00 -0300

    fix: add to string in id and recipient id

--- api/src/infra/presenters/order-presenter.ts
+++ api/src/infra/presenters/order-presenter.ts
@@ -3,9 +3,10 @@ import { Order } from '@/domain/delivery/enterprise/entities/order';
 export class OrderPresenter {
   static toHTTP(order: Order) {
     return {
-      id: order.id,
+      id: order.id.toString(),
+      trackingCode: order.trackingCode,
       title: order.title,
-      recipientId: order.recipientId,
+      recipientId: order.recipientId.toString(),
       status: order.status,
       isReturn: order.isReturned,
       city: order.city,
