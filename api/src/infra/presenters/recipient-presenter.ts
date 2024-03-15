3f13d1aa06bc4f8d4b5fa1f72ec9c357b60e0269
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-15 18:47:18 -0300

    fix: add to string in id and add recipient name

--- api/src/infra/presenters/recipient-presenter.ts
+++ api/src/infra/presenters/recipient-presenter.ts
@@ -3,7 +3,8 @@ import { Recipient } from '@/domain/delivery/enterprise/entities/recipient';
 export class RecipientPresenter {
   static toHTTP(recipient: Recipient) {
     return {
-      id: recipient.id,
+      id: recipient.id.toString(),
+      name: recipient.name,
       email: recipient.email,
       zipcode: recipient.zipcode,
       address: recipient.address,
