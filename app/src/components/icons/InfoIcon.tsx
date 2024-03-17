e9c11da8b1e3478d5eb82bd01bb01b1307142eea
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-17 20:25:16 -0300

    feat: create info icon component

---
+++ app/src/components/icons/InfoIcon.tsx
@@ -0,0 +1,23 @@
+export function InfoIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="25"
+      height="24"
+      viewBox="0 0 25 24"
+      fill="none"
+    >
+      <g clipPath="url(#clip0_44_1225)">
+        <path
+          d="M12.5 2C6.98 2 2.5 6.48 2.5 12C2.5 17.52 6.98 22 12.5 22C18.02 22 22.5 17.52 22.5 12C22.5 6.48 18.02 2 12.5 2ZM13.5 17H11.5V11H13.5V17ZM13.5 9H11.5V7H13.5V9Z"
+          fill="#FFC042"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_44_1225">
+          <rect x="0.5" width="24" height="24" rx="4" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
