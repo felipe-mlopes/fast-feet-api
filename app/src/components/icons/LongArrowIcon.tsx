d0bfc695ca75eb52517846caa3043f448355ec3e
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-17 20:26:01 -0300

    refactor: change arrow right icon component to long arrow icon component

---
+++ app/src/components/icons/LongArrowIcon.tsx
@@ -0,0 +1,16 @@
+export function LongArrowIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="80"
+      height="6"
+      viewBox="0 0 80 6"
+      fill="none"
+    >
+      <path
+        d="M0 3L5 5.88675V0.113249L0 3ZM4.5 3.5H80V2.5H4.5V3.5Z"
+        fill="#FFC042"
+      />
+    </svg>
+  );
+}
