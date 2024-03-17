c3213ca6b4ba548ac6d1ad014b96f8e07ee54543
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-17 20:18:43 -0300

    refactor: change button to link in exist icon & add section to input search & update main & include button status component

--- app/src/app/deliveries/page.tsx
+++ app/src/app/deliveries/page.tsx
@@ -1,19 +1,24 @@
-import { Button } from "@/components/Button";
+import Link from "next/link";
+
 import { Card } from "@/components/Card";
+import { ButtonStatus } from "@/components/ButtonStatus";
+
 import { ExistIcon } from "@/components/icons/ExistIcon";
 import { PinIcon } from "@/components/icons/PinIcon";
 import { SearchIcon } from "@/components/icons/SearchIcon";
 
 export default function Deliveries() {
   return (
-    <div className="flex flex-col justify-between items-center gap-24 mt-20 lg:grid lg:grid-col-2 lg:grid-row-3 lg:justify-normal">
-      <header className="space-y-8 w-full px-6">
+    <div className="flex flex-col justify-between items-center mt-20 relative h-screen lg:grid lg:grid-col-2 lg:grid-row-3 lg:justify-normal">
+      <header className="space-y-8 w-full px-6 pb-16">
         <div className="flex justify-between items-center">
           <div className="flex flex-col text-lilac-smooth">
             <p>Bem vindo,</p>
             <p>Tiago Luchtenberg</p>
           </div>
-          <ExistIcon />
+          <Link href={"/"}>
+            <ExistIcon />
+          </Link>
         </div>
         <div className="flex justify-between items-center">
           <h3 className="text-[2rem] text-white">Entregas</h3>
@@ -23,27 +28,33 @@ export default function Deliveries() {
           </div>
         </div>
       </header>
-      <main className="w-full bg-white px-6">
-        <div className="flex items-center justify-start gap-4 px-6 bg-white border border-black rounded shadow-sm">
+      <section className="flex justify-center px-6 w-full absolute top-40 z-10">
+        <div className="flex items-center justify-around gap-4 px-6 py-5 w-full bg-white border rounded shadow-card">
           <input
             type="search"
             name=""
             id=""
             placeholder="Filtrar por bairro"
-            className="px-6 py-5 bg-white"
+            className="outline-none appearance-none bg-white"
           />
           <button>
             <SearchIcon />
           </button>
         </div>
-        <div className="space-y-4">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+      </section>
+      <main className="px-6 pt-[3.25rem] pb-8 w-full h-screen bg-gray-light">
+        <div className="flex items-center gap-5 text-ligth-slate-gray">
+          <span className="content=[''] w-1/3 h-[1px] bg-bluish-gray" />
+          <p className="text-nowrap">8 entregas</p>
+          <span className="content=[''] w-1/3 h-[1px] bg-bluish-gray" />
+        </div>
+        <div className="space-y-4 pt-4">
+          {Array.from({ length: 1 }, (_, i) => i++).map((idx) => {
+            return <Card key={idx} />;
+          })}
         </div>
       </main>
-      <footer></footer>
+      <ButtonStatus />
     </div>
   );
 }
