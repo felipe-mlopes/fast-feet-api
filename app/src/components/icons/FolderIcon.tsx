40da0ce16ace517e515ce973b24bd6361476cdd6
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-17 20:25:10 -0300

    feat: create folder icon component

---
+++ app/src/components/icons/FolderIcon.tsx
@@ -0,0 +1,28 @@
+export function FolderIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="25"
+      height="24"
+      viewBox="0 0 25 24"
+      fill="none"
+    >
+      <g clipPath="url(#clip0_44_1210)">
+        <path
+          d="M19.5 3H15.32C14.9 1.84 13.8 1 12.5 1C11.2 1 10.1 1.84 9.68 3H5.5C4.4 3 3.5 3.9 3.5 5V19C3.5 20.1 4.4 21 5.5 21H19.5C20.6 21 21.5 20.1 21.5 19V5C21.5 3.9 20.6 3 19.5 3ZM12.5 3C13.05 3 13.5 3.45 13.5 4C13.5 4.55 13.05 5 12.5 5C11.95 5 11.5 4.55 11.5 4C11.5 3.45 11.95 3 12.5 3ZM14.5 17H7.5V15H14.5V17ZM17.5 13H7.5V11H17.5V13ZM17.5 9H7.5V7H17.5V9Z"
+          fill="#FFC042"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_44_1210">
+          <rect
+            width="24"
+            height="24"
+            fill="white"
+            transform="translate(0.5)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
