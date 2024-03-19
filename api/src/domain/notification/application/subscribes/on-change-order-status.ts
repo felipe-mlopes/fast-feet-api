534ef424b0d00473111ce6a6007e8ee2eb0f141b
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-19 07:15:53 -0300

    refactor: include find by orderId method of the recipient repository and send method of the send email

--- api/src/domain/notification/application/subscribes/on-change-order-status.ts
+++ api/src/domain/notification/application/subscribes/on-change-order-status.ts
@@ -3,16 +3,21 @@ import { Injectable } from '@nestjs/common';
 import { SendNotificationUseCase } from '../use-cases/send-notification';
 
 import { OrdersRepository } from '@/domain/delivery/application/repositories/orders-repository';
+import { RecipentsRepository } from '@/domain/delivery/application/repositories/recipients-repository';
 import { ChangeOrderStatusEvent } from '@/domain/delivery/enterprise/events/change-order-status-event';
+import { SendEmail } from '../mailing/sendEmail';
 
 import { EventHandler } from '@/core/events/event-handler';
 import { DomainEvents } from '@/core/events/domain-events';
+import { emailTemplate } from '@/infra/mailing/email-template';
 
 @Injectable()
 export class OnChangeOrderStatus implements EventHandler {
   constructor(
     private ordersRepository: OrdersRepository,
+    private recipientRepository: RecipentsRepository,
     private sendNotification: SendNotificationUseCase,
+    private sendEmail: SendEmail,
   ) {
     this.setupSubscriptions();
   }
@@ -31,11 +36,26 @@ export class OnChangeOrderStatus implements EventHandler {
       order.id.toString(),
     );
 
-    if (currentOrder && currentOrder.status !== 'WAITING') {
-      await this.sendNotification.execute({
-        recipientId: currentOrder.recipientId.toString(),
-        title: `O status do seu pedido ${order.title} mudou para ${order.status}`,
-      });
+    const currentRecipient = await this.recipientRepository.findByOrderId(
+      order.id.toString(),
+    );
+
+    if (currentOrder && currentRecipient && currentOrder.status !== 'WAITING') {
+      await Promise.all([
+        this.sendNotification.execute({
+          recipientId: currentOrder.recipientId.toString(),
+          title: `O status do seu pedido ${order.title} mudou para ${order.status}`,
+        }),
+
+        this.sendEmail.send({
+          to: [currentRecipient.email],
+          subject: `O status do seu pedido ${order.title} mudou para ${order.status}`,
+          html: emailTemplate({
+            recipientName: currentRecipient.name,
+            orderStatus: order.status,
+          }),
+        }),
+      ]);
     }
   }
 }
