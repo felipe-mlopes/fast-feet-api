cd9d3bdd234b7f9bd37f6dcbfeaa6b5751268039
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-19 07:17:54 -0300

    refactor: add status edit function & include recipient name + order status props as params

--- api/src/infra/mailing/email-template.ts
+++ api/src/infra/mailing/email-template.ts
@@ -1,5 +1,23 @@
-interface emailTemplateTypes {}
+interface emailTemplateProps {
+  recipientName?: string;
+  orderStatus?: string;
+}
 
-export function emailTemplate({}: emailTemplateTypes) {
-  return ``;
+export function emailTemplate({
+  recipientName,
+  orderStatus,
+}: emailTemplateProps) {
+  function statusEdit(status: unknown) {
+    if (status === 'PICKN_UP') {
+      return 'Retirado';
+    }
+
+    if (status === 'DONE') {
+      return 'Entregue';
+    }
+
+    return 'Aguardando';
+  }
+
+  return `Olá ${recipientName}! Seu pedido mudou de status para ${statusEdit(orderStatus)}`;
 }
