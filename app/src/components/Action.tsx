7084831867458dc3ea24c9a679f98fc74a0ca832
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-23 22:32:46 -0300

    feat: create action component

---
+++ app/src/components/Action.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { HTMLAttributes, useState } from "react";
+import { Button } from "./Button";
+import { Modal } from "./Modal";
+
+interface ActionProps extends HTMLAttributes<HTMLDivElement> {
+  buttonContent: string;
+  modalContent: string;
+  isDisable?: boolean;
+}
+
+export function Action({
+  buttonContent,
+  modalContent,
+  isDisable,
+  ...props
+}: ActionProps) {
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  const handleModalClose = () => {
+    console.log("clicou");
+    setIsModalOpen(!isModalOpen);
+  };
+
+  return (
+    <div {...props}>
+      <Button content={buttonContent} disabled={isDisable} />
+      <Modal
+        type="package"
+        content={modalContent}
+        isDone={false}
+        isOpen={isModalOpen}
+        onClose={handleModalClose}
+      />
+    </div>
+  );
+}
