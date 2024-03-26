b5a10fd6e252678001f5e15c0929cbea761c9b64
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-25 22:52:28 -0300

    refactor: include isDone condition before of the div parent

--- app/src/components/Action.tsx
+++ app/src/components/Action.tsx
@@ -8,30 +8,36 @@ interface ActionProps extends HTMLAttributes<HTMLDivElement> {
   buttonContent: string;
   modalContent: string;
   isDisable?: boolean;
+  isDone: boolean;
 }
 
 export function Action({
   buttonContent,
   modalContent,
   isDisable,
+  isDone,
   ...props
 }: ActionProps) {
-  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleModalClose = () => {
     setIsModalOpen(!isModalOpen);
   };
 
   return (
-    <div {...props}>
-      <Button content={buttonContent} disabled={isDisable} />
-      <Modal
-        type="package"
-        content={modalContent}
-        isDone={false}
-        isOpen={isModalOpen}
-        onClose={handleModalClose}
-      />
-    </div>
+    <>
+      {!isDone && (
+        <div {...props}>
+          <Button content={buttonContent} disabled={isDisable} />
+          <Modal
+            type="package"
+            content={modalContent}
+            isDone={isDone}
+            isOpen={isModalOpen}
+            onClose={handleModalClose}
+          />
+        </div>
+      )}
+    </>
   );
 }
