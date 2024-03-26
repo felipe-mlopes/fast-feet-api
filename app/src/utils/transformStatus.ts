3338bf40176bd420374747ea538bf002f40b2858
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-25 22:52:44 -0300

    feat: create status edit function

--- app/src/utils/transformStatus.ts
+++ app/src/utils/transformStatus.ts
@@ -9,3 +9,17 @@ export function transformStatus(status: string): string {
         return status;
     }
   }
+
+
+  export function statusEdit(status: string): string {
+    switch (status) {
+      case "WAITING":
+        return "aguardando";
+      case "PICKN_UP":
+        return "retirado";
+      case "DONE":
+        return "entregue";
+      default:
+        return status;
+    }
+  }
