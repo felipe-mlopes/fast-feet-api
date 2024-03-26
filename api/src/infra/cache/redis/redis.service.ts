a008da9861a17c557fdda3b488ed4fc496492ec2
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-25 22:44:37 -0300

    fix: add event listener on to check the connection to redis

--- api/src/infra/cache/redis/redis.service.ts
+++ api/src/infra/cache/redis/redis.service.ts
@@ -5,11 +5,35 @@ import { EnvService } from '@/infra/env/env.service';
 
 @Injectable()
 export class RedisService extends Redis implements OnModuleDestroy {
+  private nextErrorMessageTime: Date | null = null;
+
   constructor(envService: EnvService) {
     super({
       host: envService.get('REDIS_HOST'),
       port: envService.get('REDIS_PORT'),
       db: envService.get('REDIS_DB'),
+    });
+
+    this.on('error', (error) => {
+      const currentDate = new Date();
+
+      if (
+        !this.nextErrorMessageTime ||
+        currentDate > this.nextErrorMessageTime
+      ) {
+        const nextErrorMessageAlert = new Date();
+        nextErrorMessageAlert.setMinutes(
+          nextErrorMessageAlert.getMinutes() + 5,
+        );
+
+        this.nextErrorMessageTime = nextErrorMessageAlert;
+
+        console.error('Redis connection error:', error);
+      }
+    });
+
+    this.on('ready', () => {
+      this.nextErrorMessageTime = null;
     });
   }
 
