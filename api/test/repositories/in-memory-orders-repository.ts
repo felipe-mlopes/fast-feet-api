57a74a9f708bb2274817a9d917cb60dfb0e435aa
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-29 12:34:39 -0300

    test: refactor to add findDetailsById method and update findManyRecentByCityAndOrdersWaitingAndPicknUp and findManyRecentByCityAndOrdersDone method

--- api/test/repositories/in-memory-orders-repository.ts
+++ api/test/repositories/in-memory-orders-repository.ts
@@ -1,10 +1,16 @@
 import { OrdersRepository } from '@/domain/delivery/application/repositories/orders-repository';
 import { Order, Status } from '@/domain/delivery/enterprise/entities/order';
+
+import { InMemoryRecipientsRepository } from './in-memory-recipients-repository';
+
 import { PaginationParams } from '@/core/repositories/pagination-params';
 import { DomainEvents } from '@/core/events/domain-events';
+import { OrderDetails } from '@/domain/delivery/enterprise/entities/value-objects/order-details';
 
 export class InMemoryOrdersRepository implements OrdersRepository {
   public items: Order[] = [];
+
+  constructor(private recipientsRepository: InMemoryRecipientsRepository) {}
 
   async findById(id: string) {
     const order = this.items.find((item) => item.id.toString() === id);
@@ -14,6 +20,43 @@ export class InMemoryOrdersRepository implements OrdersRepository {
     }
 
     return order;
+  }
+
+  async findDetailsById(id: string): Promise<Order | null> {
+    const order = this.items.find((item) => item.id.toString() === id);
+
+    if (!order) {
+      return null;
+    }
+
+    const recipient = this.recipientsRepository.items.find((shipping) => {
+      return shipping.id.equals(order.recipientId);
+    });
+
+    if (!recipient) {
+      throw new Error(
+        `Recipient with ID "${order.recipientId.toString()}" does not exist.`,
+      );
+    }
+
+    return OrderDetails.create({
+      orderId: order.id,
+      trackingCode: order.trackingCode,
+      title: order.title,
+      status: order.status,
+      isReturned: order.isReturned,
+      recipientId: order.recipientId,
+      recipientName: recipient.name,
+      recipientAddress: recipient.address,
+      recipientZipcode: recipient.zipcode,
+      recipientState: recipient.state,
+      recipientCity: recipient.city,
+      recipientNeighborhood: recipient.neighborhood,
+      createdAt: order.createdAt,
+      deliverymanId: order.deliverymanId,
+      picknUpAt: order.picknUpAt,
+      deliveryAt: order.deliveryAt,
+    });
   }
 
   async findByTrackingCode(trackingCode: string): Promise<Order | null> {
@@ -30,14 +73,23 @@ export class InMemoryOrdersRepository implements OrdersRepository {
     city: string,
     deliverymanId: string,
     { page }: PaginationParams,
-  ) {
-    const orders = this.items
-      .filter((item) => {
+  ): Promise<Order[] | null> {
+    const recipient = this.recipientsRepository.items.filter(
+      (shipping) => shipping.city === city,
+    );
+
+    if (recipient.length < 1) return null;
+
+    const ordersWithRecipient = this.items.filter((item) => {
+      return recipient.find((shipping) => shipping.id === item.recipientId);
+    });
+
+    const orders = ordersWithRecipient
+      .filter((order) => {
         return (
-          (item.status === Status.WAITING && item.city === city) ||
-          (item.status === Status.PICKN_UP &&
-            item.deliverymanId?.toString() === deliverymanId &&
-            item.city === city)
+          order.status === Status.WAITING ||
+          (order.status === Status.PICKN_UP &&
+            order.deliverymanId?.toString() === deliverymanId)
         );
       })
       .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
@@ -51,13 +103,23 @@ export class InMemoryOrdersRepository implements OrdersRepository {
     deliverymanId: string,
     { page }: PaginationParams,
   ) {
-    const orders = this.items
-      .filter(
-        (item) =>
-          item.status === Status.DONE &&
-          item.city === city &&
-          item.deliverymanId?.toString() === deliverymanId,
-      )
+    const recipient = this.recipientsRepository.items.filter(
+      (shipping) => shipping.city === city,
+    );
+
+    if (recipient.length < 1) return null;
+
+    const ordersWithRecipient = this.items.filter((item) => {
+      return recipient.find((shipping) => shipping.id === item.recipientId);
+    });
+
+    const orders = ordersWithRecipient
+      .filter((order) => {
+        return (
+          order.status === Status.DONE &&
+          order.deliverymanId?.toString() === deliverymanId
+        );
+      })
       .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
       .slice((page - 1) * 20, page * 20);
 
