0d1176df4f7fe2ff20c22c9a2f6c8e186f310c4a
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-30 11:56:01 -0300

    refactor: change Order to OrderDetails in findDetailsById and Order to OrderWithNeighborhood in findManyRecentByCityAndOrdersWaitingAndPicknUp and findManyRecentByCityAndOrdersDone

--- api/src/domain/delivery/application/repositories/orders-repository.ts
+++ api/src/domain/delivery/application/repositories/orders-repository.ts
@@ -1,20 +1,23 @@
-import { PaginationParams } from '@/core/repositories/pagination-params';
 import { Order } from '@/domain/delivery/enterprise/entities/order';
+import { OrderWithNeighborhood } from '../../enterprise/entities/value-objects/order-with-neighborhood';
+import { OrderDetails } from '../../enterprise/entities/value-objects/order-details';
+
+import { PaginationParams } from '@/core/repositories/pagination-params';
 
 export abstract class OrdersRepository {
   abstract findById(id: string): Promise<Order | null>;
-  abstract findDetailsById(id: string): Promise<Order | null>;
+  abstract findDetailsById(id: string): Promise<OrderDetails | null>;
   abstract findByTrackingCode(trackingCode: string): Promise<Order | null>;
   abstract findManyRecentByCityAndOrdersWaitingAndPicknUp(
     city: string,
     deliverymanId: string,
     params: PaginationParams,
-  ): Promise<Order[] | null>;
+  ): Promise<OrderWithNeighborhood[] | null>;
   abstract findManyRecentByCityAndOrdersDone(
     city: string,
     deliverymanId: string,
     params: PaginationParams,
-  ): Promise<Order[] | null>;
+  ): Promise<OrderWithNeighborhood[] | null>;
   abstract create(order: Order): Promise<void>;
   abstract save(order: Order): Promise<void>;
 }
