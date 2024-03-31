8f123e94da92a08894f5b88e72484089817a4adb
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-03-31 10:24:30 -0300

    feat: create recipients props in data types

---
+++ app/src/data/types/recipients.d.ts
@@ -0,0 +1,10 @@
+export interface RecipientsProps {
+    id: string
+    name: string
+    email: string
+    zipcode: number
+    address: string
+    neighborhood: string,
+    city: string,
+    state: string
+}
