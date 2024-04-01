64d1a121962c293b43f57f4eecea6052a85391fb
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-01 08:09:13 -0300

    feat: create get deliveryman by id controller

---
+++ api/src/infra/http/controllers/get-deliveryman-by-id.controller.ts
@@ -0,0 +1,31 @@
+import { BadRequestException, Controller, Get } from '@nestjs/common';
+
+import { GetDeliverymanByIdUseCase } from '@/domain/delivery/application/use-cases/get-deliveryman-by-id';
+
+import { CurrentUser } from '@/infra/auth/current-user.decorator';
+import { UserPayload } from '@/infra/auth/jwt.strategy';
+import { DeliverymanPresenter } from '@/infra/presenters/deliveryman-presenter';
+
+@Controller('/deliveryman')
+export class GetDeliverymanByIdController {
+  constructor(private getDeliverymanById: GetDeliverymanByIdUseCase) {}
+
+  @Get()
+  async handle(@CurrentUser() user: UserPayload) {
+    const deliverymanId = user.sub;
+
+    const result = await this.getDeliverymanById.execute({
+      deliverymanId,
+    });
+
+    if (result.isLeft()) {
+      throw new BadRequestException();
+    }
+
+    const { deliveryman } = result.value;
+
+    return {
+      deliveryman: DeliverymanPresenter.toHTTP(deliveryman),
+    };
+  }
+}
