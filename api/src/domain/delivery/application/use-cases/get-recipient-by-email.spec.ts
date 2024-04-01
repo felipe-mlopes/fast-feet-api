f5e28a231c9ab60dd674b1f7d0b832a1c94af12f
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-01 08:11:46 -0300

    test: refactor to change describe name, add makeAdminUser and InMemoryAdminRepository and include adminRole on result

--- api/src/domain/delivery/application/use-cases/get-recipient-by-email.spec.ts
+++ api/src/domain/delivery/application/use-cases/get-recipient-by-email.spec.ts
@@ -1,21 +1,31 @@
 import { GetRecipientByEmailUseCase } from './get-recipient-by-email';
 
+import { InMemoryAdminRepository } from 'test/repositories/in-memory-admin-repository';
 import { InMemoryRecipientsRepository } from 'test/repositories/in-memory-recipients-repository';
 
+import { makeAdminUser } from 'test/factories/make-admin-user';
 import { makeRecipient } from 'test/factories/make-recipient';
 
-import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error';
+import { Role } from '../../enterprise/entities/order';
 
+import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error';
+import { NotAllowedError } from '@/core/errors/not-allowed-error';
+
+let inMemoryAdminRepository: InMemoryAdminRepository;
 let inMemoryRecipientsRepository: InMemoryRecipientsRepository;
 let sut: GetRecipientByEmailUseCase;
 
-describe('Get Order by Tracking Code', () => {
+describe('Get Recipient by Email', () => {
   beforeEach(() => {
+    inMemoryAdminRepository = new InMemoryAdminRepository();
     inMemoryRecipientsRepository = new InMemoryRecipientsRepository();
     sut = new GetRecipientByEmailUseCase(inMemoryRecipientsRepository);
   });
 
-  it('should be able to get a order by tracking code', async () => {
+  it('should be able to get a recipient by email', async () => {
+    const admin = makeAdminUser();
+    await inMemoryAdminRepository.create(admin);
+
     const recipient = makeRecipient({
       name: 'John Doe',
       email: 'john-doe@example.com',
@@ -24,21 +34,39 @@ describe('Get Order by Tracking Code', () => {
 
     const result = await sut.execute({
       recipientEmail: 'john-doe@example.com',
+      adminRole: admin.role,
     });
 
     expect(result.isRight()).toBe(true);
     expect(inMemoryRecipientsRepository.items[0].name).toBe('John Doe');
   });
 
-  it('should not be able to get a order by the missing tracking code', async () => {
+  it('should not be able to get a recipient by wrong email', async () => {
+    const admin = makeAdminUser();
+    await inMemoryAdminRepository.create(admin);
+
     const recipient = makeRecipient();
     await inMemoryRecipientsRepository.create(recipient);
 
     const result = await sut.execute({
       recipientEmail: 'john-doe@example.com',
+      adminRole: admin.role,
     });
 
     expect(result.isLeft()).toBe(true);
     expect(result.value).toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it('should not be able to get a recipient by email without being admin', async () => {
+    const recipient = makeRecipient({ email: 'john-doe@example.com' });
+    await inMemoryRecipientsRepository.create(recipient);
+
+    const result = await sut.execute({
+      recipientEmail: recipient.email,
+      adminRole: Role.DELIVERYMAN,
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(NotAllowedError);
   });
 });
