2eb2ee8ad25c065b148b870a2f24e6fa6c220ae5
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-01 08:12:12 -0300

    test: create e2e test to get recipient by email controller

---
+++ api/src/infra/http/controllers/get-recipient-by-email.controller.e2e-spec.ts
@@ -0,0 +1,58 @@
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { JwtService } from '@nestjs/jwt';
+import request from 'supertest';
+
+import { AppModule } from '@/infra/app.module';
+import { DatabaseModule } from '@/infra/database/database.module';
+
+import { RecipientFactory } from 'test/factories/make-recipient';
+import { AdminUserFactory } from 'test/factories/make-admin-user';
+
+describe('Get Recipient by Email (E2E)', () => {
+  let app: INestApplication;
+  let recipientFactory: RecipientFactory;
+  let adminUserFactory: AdminUserFactory;
+  let jwt: JwtService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [RecipientFactory, AdminUserFactory],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    recipientFactory = moduleRef.get(RecipientFactory);
+    adminUserFactory = moduleRef.get(AdminUserFactory);
+    jwt = moduleRef.get(JwtService);
+
+    await app.init();
+  });
+
+  test('[GET] /recipient', async () => {
+    const recipient = await recipientFactory.makePrismaRecipient({
+      name: 'John Doe',
+      email: 'john-doe@example.com',
+    });
+
+    const admin = await adminUserFactory.makePrismaAdminUser();
+
+    const accessToken = jwt.sign({
+      sub: admin.id.toString(),
+      role: admin.role,
+    });
+
+    const response = await request(app.getHttpServer())
+      .get(`/recipient?recipientEmail=${recipient.email}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send();
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.recipient).toEqual(
+      expect.objectContaining({
+        name: 'John Doe',
+      }),
+    );
+  });
+});
