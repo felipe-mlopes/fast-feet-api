1111eac1caf0f71b5aac56eba1d1f63cca3e5f2f
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-01 08:08:20 -0300

    feat: include adminRole on request and add NotAllowedError on response

--- api/src/domain/delivery/application/use-cases/get-recipient-by-email.ts
+++ api/src/domain/delivery/application/use-cases/get-recipient-by-email.ts
@@ -3,16 +3,19 @@ import { Injectable } from '@nestjs/common';
 import { RecipentsRepository } from '../repositories/recipients-repository';
 
 import { Recipient } from '../../enterprise/entities/recipient';
+import { Role } from '../../enterprise/entities/order';
 
 import { Either, left, right } from '@/core/either';
 import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error';
+import { NotAllowedError } from '@/core/errors/not-allowed-error';
 
 interface GetRecipientByEmailRequest {
   recipientEmail: string;
+  adminRole: string;
 }
 
 type GetRecipientByEmailResponse = Either<
-  ResourceNotFoundError,
+  ResourceNotFoundError | NotAllowedError,
   {
     recipient: Recipient;
   }
@@ -24,7 +27,12 @@ export class GetRecipientByEmailUseCase {
 
   async execute({
     recipientEmail,
+    adminRole,
   }: GetRecipientByEmailRequest): Promise<GetRecipientByEmailResponse> {
+    if (adminRole !== Role.ADMIN) {
+      return left(new NotAllowedError());
+    }
+
     const recipient =
       await this.recipientsRepository.findByEmail(recipientEmail);
 
