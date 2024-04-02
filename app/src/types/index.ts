d335ace4dd100438746c61b6a291a507db898757
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-02 05:59:06 -0300

    refactor: remove app files and move to fast-feet-app

--- app/src/types/index.ts
+++
@@ -1,8 +0,0 @@
-import { ZodIssue } from "zod"
-
-export type ValidationError = Partial<Pick<ZodIssue, 'path' | 'message'>>
-
-export interface FormStateTypes {
-    data?: string | null
-    error?: ValidationError[] | null
-}
