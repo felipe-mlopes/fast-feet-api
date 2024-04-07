efa5b8c1ec4c74a1dff5c03a6410a4cbbff299a6
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-07 15:26:58 -0300

    feat: add OrderDetailsResponse and OrderDetailsResponseDto classes

--- src/infra/presenters/order-details-presenter.ts
+++ src/infra/presenters/order-details-presenter.ts
@@ -1,9 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import { OrderDetails } from '@/domain/delivery/enterprise/entities/value-objects/order-details';
 
 export class OrderDetailsPresenter {
   static toHTTP(order: OrderDetails) {
     return {
-      id: order.orderId,
+      id: order.orderId.toString,
       trackingCode: order.trackingCode,
       title: order.title,
       status: order.status,
@@ -21,4 +23,59 @@ export class OrderDetailsPresenter {
       deliveryAt: order.deliveryAt,
     };
   }
+}
+
+class OrderDetailsResponse {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  trackingCode: string;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty()
+  status: string;
+
+  @ApiProperty()
+  isReturn: boolean;
+
+  @ApiProperty()
+  recipientId: string;
+
+  @ApiProperty()
+  recipientName: string;
+
+  @ApiProperty()
+  recipientAddress: string;
+
+  @ApiProperty()
+  recipientZipcode: number;
+
+  @ApiProperty()
+  recipientState: string;
+
+  @ApiProperty()
+  recipientCity: string;
+
+  @ApiProperty()
+  recipientNeighborhood: string;
+
+  @ApiProperty()
+  deliverymanId: string;
+
+  @ApiProperty()
+  createdAt: string;
+
+  @ApiProperty()
+  picknUpAt: string;
+
+  @ApiProperty()
+  deliveryAt: string;
+}
+
+export class OrderDetailsResponseDto {
+  @ApiProperty({ type: OrderDetailsResponse })
+  order: OrderDetailsResponse;
 }
