668b1472d738e163b5842f5816714fd4cad6d811
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-07 15:18:24 -0300

    feat: add ApiBearerAuth, ApiOperation and ApiResponse of the swagger & include OrderWithNeighborhoodPresenter as handle promise response

--- src/infra/http/controllers/fetch-nearby-orders-done.controller.ts
+++ src/infra/http/controllers/fetch-nearby-orders-done.controller.ts
@@ -1,28 +1,47 @@
 import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 import { CurrentUser } from '@/infra/auth/current-user.decorator';
 import { UserPayload } from '@/infra/auth/jwt.strategy';
-import { OrderWithNeighborhoodPresenter } from '@/infra/presenters/order-with-neighborhood-presenter';
+import {
+  OrderWithNeighborhoodPresenter,
+  OrderWithNeighborhoodResponseDto,
+} from '@/infra/presenters/order-with-neighborhood-presenter';
 
 import { PageQueryParamsDto } from '../dto/page-query-params.dto';
 
 import { FetchNearbyOrdersDoneUseCase } from '@/domain/delivery/application/use-cases/fetch-nearby-orders-done';
 
 @ApiTags('orders')
+@ApiBearerAuth('deliverymanToken')
 @Controller('/orders/done')
 export class FecthNearbyOrdersDoneController {
   constructor(private fetchNearbyOrdersDone: FetchNearbyOrdersDoneUseCase) {}
 
+  @ApiOperation({
+    summary: 'Fetch recent orders done',
+    description:
+      'Fetch recent orders done by logged in deliveryman from the same city',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Orders with done status',
+    type: OrderWithNeighborhoodResponseDto,
+  })
   @Get()
   async handle(
-    @Query('page')
-    page: PageQueryParamsDto,
     @Query('city')
     city: string,
+    @Query('page')
+    page: PageQueryParamsDto,
     @CurrentUser()
     user: UserPayload,
-  ) {
+  ): Promise<OrderWithNeighborhoodPresenter> {
     const deliverymanId = user.sub;
 
     const result = await this.fetchNearbyOrdersDone.execute({
--- src/infra/http/controllers/fetch-nearby-orders-waiting-and-pickn-up.controller.ts
+++ src/infra/http/controllers/fetch-nearby-orders-waiting-and-pickn-up.controller.ts
@@ -1,21 +1,41 @@
 import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 import { CurrentUser } from '@/infra/auth/current-user.decorator';
 import { UserPayload } from '@/infra/auth/jwt.strategy';
-import { OrderWithNeighborhoodPresenter } from '@/infra/presenters/order-with-neighborhood-presenter';
+import {
+  OrderWithNeighborhoodPresenter,
+  OrderWithNeighborhoodResponseDto,
+} from '@/infra/presenters/order-with-neighborhood-presenter';
 
 import { PageQueryParamsDto } from '../dto/page-query-params.dto';
 
 import { FetchNearbyOrdersWaitingAndPicknUpUseCase } from '@/domain/delivery/application/use-cases/fetch-nearby-orders-waiting-and-pickn-up';
 
 @ApiTags('orders')
+@ApiBearerAuth('deliverymanToken')
 @Controller('/orders/pending')
 export class FecthNearbyOrdersWaitingAndPicknUpController {
   constructor(
     private fetchNearbyOrdersWaitingOrPicknUp: FetchNearbyOrdersWaitingAndPicknUpUseCase,
   ) {}
 
+  @ApiOperation({
+    summary: 'Fetch recent orders pending',
+    description:
+      'Fetch recent orders of the same city with waiting status and those picked up by deliveryman logged in.',
+  })
+  @ApiResponse({
+    status: 200,
+    description:
+      'Orders with waiting status and those picked up by deliveryman logged in.',
+    type: OrderWithNeighborhoodResponseDto,
+  })
   @Get()
   async handle(
     @Query('city')
@@ -23,7 +43,7 @@ export class FecthNearbyOrdersWaitingAndPicknUpController {
     @Query('page')
     page: PageQueryParamsDto,
     @CurrentUser() user: UserPayload,
-  ) {
+  ): Promise<OrderWithNeighborhoodPresenter> {
     const deliverymanId = user.sub;
 
     const result = await this.fetchNearbyOrdersWaitingOrPicknUp.execute({
