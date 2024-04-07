2cb560399c51b43d0c9b2a9aa9355718c768ab44
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-07 15:27:20 -0300

    feat: add OrderResponse and OrderResponseDto classes

--- src/infra/presenters/order-presenter.ts
+++ src/infra/presenters/order-presenter.ts
@@ -1,3 +1,5 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import { Order } from '@/domain/delivery/enterprise/entities/order';
 
 export class OrderPresenter {
@@ -6,13 +8,50 @@ export class OrderPresenter {
       id: order.id.toString(),
       trackingCode: order.trackingCode,
       title: order.title,
-      recipientId: order.recipientId.toString(),
       status: order.status,
       isReturn: order.isReturned,
+      recipientId: order.recipientId.toString(),
       deliverymanId: order.deliverymanId?.toString(),
       createdAt: order.createdAt,
       picknUpAt: order.picknUpAt,
       deliveryAt: order.deliveryAt,
     };
   }
+}
+
+class OrderResponse {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  trackingCode: string;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty()
+  status: string;
+
+  @ApiProperty()
+  isReturn: boolean;
+
+  @ApiProperty()
+  recipientId: string;
+
+  @ApiProperty()
+  deliverymanId: string;
+
+  @ApiProperty()
+  createdAt: string;
+
+  @ApiProperty()
+  picknUpAt: string;
+
+  @ApiProperty()
+  deliveryAt: string;
+}
+
+export class OrderResponseDto {
+  @ApiProperty({ type: OrderResponse })
+  order: OrderResponse;
 }
