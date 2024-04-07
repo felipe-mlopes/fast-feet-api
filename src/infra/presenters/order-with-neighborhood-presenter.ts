b20fdea265a15ed6533fb4b2273f0060a1f7d698
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-07 15:27:59 -0300

    feat: add OrderWithNeighborhoodResponse and OrderWithNeighborhoodResponseDto classes

--- src/infra/presenters/order-with-neighborhood-presenter.ts
+++ src/infra/presenters/order-with-neighborhood-presenter.ts
@@ -1,3 +1,5 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import { OrderWithNeighborhood } from '@/domain/delivery/enterprise/entities/value-objects/order-with-neighborhood';
 
 export class OrderWithNeighborhoodPresenter {
@@ -13,4 +15,38 @@ export class OrderWithNeighborhoodPresenter {
       createdAt: order.createdAt,
     };
   }
+}
+
+class OrderWithNeighborhoodResponse {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  trackingCode: string;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty()
+  status: string;
+
+  @ApiProperty()
+  isReturn: boolean;
+
+  @ApiProperty()
+  recipientId: string;
+
+  @ApiProperty()
+  recipientNeighborhood: string;
+
+  @ApiProperty()
+  createdAt: string;
+}
+
+export class OrderWithNeighborhoodResponseDto {
+  @ApiProperty({
+    type: OrderWithNeighborhoodResponse,
+    isArray: true,
+  })
+  orders: OrderWithNeighborhoodResponse[];
 }
