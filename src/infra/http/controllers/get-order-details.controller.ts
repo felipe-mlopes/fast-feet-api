79722ca64924b2e63e139325a9a25c80634f29f7
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-07 15:14:25 -0300

    feat: add ApiBearerAuth, ApiOperation and ApiResponse of the swagger

--- src/infra/http/controllers/get-order-details.controller.ts
+++ src/infra/http/controllers/get-order-details.controller.ts
@@ -1,15 +1,32 @@
 import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+
+import {
+  OrderDetailsPresenter,
+  OrderDetailsResponseDto,
+} from '@/infra/presenters/order-details-presenter';
 
 import { GetOrderDetailsUseCase } from '@/domain/delivery/application/use-cases/get-order-details';
 
-import { OrderDetailsPresenter } from '@/infra/presenters/order-details-presenter';
-
 @ApiTags('orders')
+@ApiBearerAuth('deliverymanToken')
 @Controller('/orders/:orderId')
 export class GetOrderDetailsController {
   constructor(private getOrderDetails: GetOrderDetailsUseCase) {}
 
+  @ApiOperation({
+    summary: 'Get order details',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Order details.',
+    type: OrderDetailsResponseDto,
+  })
   @Get()
   async handle(@Param('orderId') orderId: string) {
     const result = await this.getOrderDetails.execute({
--- src/infra/http/controllers/get-recipient-by-email.controller.ts
+++ src/infra/http/controllers/get-recipient-by-email.controller.ts
@@ -1,17 +1,34 @@
 import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 import { CurrentUser } from '@/infra/auth/current-user.decorator';
 import { UserPayload } from '@/infra/auth/jwt.strategy';
-import { RecipientPresenter } from '@/infra/presenters/recipient-presenter';
+import {
+  RecipientPresenter,
+  RecipientResponseDto,
+} from '@/infra/presenters/recipient-presenter';
 
 import { GetRecipientByEmailUseCase } from '@/domain/delivery/application/use-cases/get-recipient-by-email';
 
 @ApiTags('recipient')
+@ApiBearerAuth('adminToken')
 @Controller('/recipient')
 export class GetRecipientByEmailController {
   constructor(private getRecipientByEmail: GetRecipientByEmailUseCase) {}
 
+  @ApiOperation({
+    summary: 'Get recipient by email',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Recipient',
+    type: RecipientResponseDto,
+  })
   @Get()
   async handle(
     @Query('recipientEmail') recipientEmail: string,
