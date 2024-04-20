4a8fbd21a88c4131a4677fa1e8dad7ceaa35485a
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-20 13:42:16 -0300

    docs: change name user model map

---
+++ prisma/migrations/20240420163920_update/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "orders" DROP CONSTRAINT "orders_deliveryman_id_fkey";
+
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'DELIVERYMAN',
+    "cpf" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_cpf_key" ON "user"("cpf");
+
+-- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_deliveryman_id_fkey" FOREIGN KEY ("deliveryman_id") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
