56c5cb35f5f595fa66bfefda3756d3e745441228
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-23 22:16:09 -0300

    styles: add space between RecipientEmailProps interface and RecipientEmail class

--- src/domain/delivery/enterprise/entities/value-objects/recipient-email.ts
+++ src/domain/delivery/enterprise/entities/value-objects/recipient-email.ts
@@ -3,6 +3,7 @@ import { ValueObject } from '@/core/entities/value-object';
 export interface RecipientEmailProps {
   email: string;
 }
+
 export class RecipientEmail extends ValueObject<RecipientEmailProps> {
   get email() {
     return this.props.email;
