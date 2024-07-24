d5d108c5e5e2c0ddaed7dcfea885828045019be6
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-07-24 09:10:04 -0300

    docs: add ApiExcludeController in HealthController

--- src/infra/http/controllers/health.controller.ts
+++ src/infra/http/controllers/health.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, HttpCode } from '@nestjs/common';
+import { ApiExcludeController } from '@nestjs/swagger';
 
 import { Public } from '@/infra/auth/public';
 
 @Controller('/health')
 @Public()
+@ApiExcludeController()
 export class HealthController {
   @Get()
   @HttpCode(200)
